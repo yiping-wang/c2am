5c835996888d74a390dde1cf260bd2df327091c4
Yiping Wang <yiping.wang@gmx.com>
2022-01-23 16:26:10 -0500

    .

--- train_cam.py
+++ train_cam.py
@@ -130,5 +130,6 @@ if __name__ == '__main__':
         device = pyutils.set_gpus(n_gpus=1)
     else:
         device = 'cpu'
+    print(device)
     config = pyutils.parse_config(args.config)
     train(config, device)
