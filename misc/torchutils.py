85dcb2ebae9f32daf6037a725e36b13b233a5e73
Yiping Wang <yiping.wang@gmx.com>
2022-01-25 13:25:48 -0500

    .

--- misc/torchutils.py
+++ misc/torchutils.py
@@ -59,5 +59,6 @@ def gap2d(x, keepdims=False):
     return out
 
 def lse_agg(cam, r):
-    h,w = cam.shape[-2:]
-    return (1/r) * torch.log((1/(h * w)) * torch.sum(torch.exp(cam * r), dim=(2,3))) 
+    # h,w = cam.shape[-2:]
+    return (1/r) * torch.logsumexp(cam * r, dim=(2,3))
+    #return (1/r) * torch.log((1/(h * w)) * torch.sum(torch.exp(cam * r), dim=(2,3))) 
--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -55,8 +55,8 @@ class CAM(Net):
         x = self.stage2(x)
         x = self.stage3(x)
         x = self.stage4(x)
-        # x = self.classifier(x)
-        x = F.conv2d(x, self.classifier.weight)
+        x = self.classifier(x)
+        # x = F.conv2d(x, self.classifier.weight)
         x = F.relu(x)
         x = x[0] + x[1].flip(-1)
         return x
