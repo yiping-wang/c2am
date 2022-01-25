2c4f038153351b5dc6fc6c541b33eb019165806b
Yiping Wang <yiping.wang@gmx.com>
2022-01-25 15:43:20 -0500

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -67,8 +67,7 @@ class CAM(nn.Module):
         x = self.stage2(x)
         x = self.stage3(x)
         x = self.stage4(x)
-        x = self.classifier(x)
-        # x = F.conv2d(x, self.classifier.weight)
+        x = F.conv2d(x, self.classifier.weight)
         x = F.relu(x)
         x = x[0] + x[1].flip(-1)
         return x
