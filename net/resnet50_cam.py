447104f4a29d686ea4feb5deb0d2ddc1eaca5e3b
Yiping Wang <yiping.wang@gmx.com>
2022-01-25 13:33:23 -0500

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -43,12 +43,24 @@ class Net(nn.Module):
         return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
 
 
-class CAM(Net):
+class CAM(nn.Module):
     def __init__(self):
         super(CAM, self).__init__()
-        # for i in self.backbone:
-        #     for p in i.parameters():
-        #         p.requires_grad = False
+
+        self.resnet50 = resnet50.resnet50(
+            pretrained=True, strides=(2, 2, 2, 1))
+
+        self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1, self.resnet50.relu, self.resnet50.maxpool,
+                                    self.resnet50.layer1)
+        self.stage2 = nn.Sequential(self.resnet50.layer2)
+        self.stage3 = nn.Sequential(self.resnet50.layer3)
+        self.stage4 = nn.Sequential(self.resnet50.layer4)
+
+        self.classifier = nn.Conv2d(2048, 20, 1, bias=False)
+
+        self.backbone = nn.ModuleList(
+            [self.stage1, self.stage2, self.stage3, self.stage4])
+        self.newly_added = nn.ModuleList([self.classifier])
 
     def forward(self, x):
         x = self.stage1(x)
@@ -61,9 +73,36 @@ class CAM(Net):
         x = x[0] + x[1].flip(-1)
         return x
 
-    def eval(self):
-        self.backbone = self.backbone.eval()
-        self.newly_added = self.newly_added.eval()
+    def train(self, mode=True):
+        for p in self.resnet50.conv1.parameters():
+            p.requires_grad = False
+        for p in self.resnet50.bn1.parameters():
+            p.requires_grad = False
 
     def trainable_parameters(self):
         return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
+
+# class CAM(Net):
+#     def __init__(self):
+#         super(CAM, self).__init__()
+#         # for i in self.backbone:
+#         #     for p in i.parameters():
+#         #         p.requires_grad = False
+
+#     def forward(self, x):
+#         x = self.stage1(x)
+#         x = self.stage2(x)
+#         x = self.stage3(x)
+#         x = self.stage4(x)
+#         x = self.classifier(x)
+#         # x = F.conv2d(x, self.classifier.weight)
+#         x = F.relu(x)
+#         x = x[0] + x[1].flip(-1)
+#         return x
+
+#     def eval(self):
+#         self.backbone = self.backbone.eval()
+#         self.newly_added = self.newly_added.eval()
+
+#     def trainable_parameters(self):
+#         return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
