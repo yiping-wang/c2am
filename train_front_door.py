af66887bab4004154410c604f5728e72e0db76d2
Yiping Wang <yiping.wang@gmx.com>
2022-01-25 19:50:21 -0500

    .

--- train_front_door.py
+++ train_front_door.py
@@ -132,14 +132,14 @@ def train(config, device):
             strided_size = imutils.get_strided_size(
                 (image_size_height, image_size_width), 4)
             cams = []
-            with torch.no_grad():
-                for b in range(cam_batch_size):
-                    strided_cam = cam_model(imgs[b])
-                    # strided_cam = F.interpolate(torch.unsqueeze(
-                    #     strided_cam, 0), strided_size, mode='bilinear', align_corners=False)[0]
-                    strided_cam = strided_cam / \
-                        (F.adaptive_max_pool2d(strided_cam.detach(), (1, 1)) + 1e-5)
-                    cams += [strided_cam.unsqueeze(0)]
+
+            for b in range(cam_batch_size):
+                strided_cam = cam_model(imgs[b])
+                # strided_cam = F.interpolate(torch.unsqueeze(
+                #     strided_cam, 0), strided_size, mode='bilinear', align_corners=False)[0]
+                strided_cam = strided_cam / \
+                    (F.adaptive_max_pool2d(strided_cam.detach(), (1, 1)) + 1e-5)
+                cams += [strided_cam.unsqueeze(0)]
 
             acams = torch.cat(cams, dim=0)  # B * 20 * H * W
 
