2758d01d5dff5befd6850085546f66e450dcf90a
Yiping Wang <yiping.wang@gmx.com>
2022-01-27 15:12:57 -0500

    .

--- train_cam.py
+++ train_cam.py
@@ -108,8 +108,7 @@ def train(config, device):
                 # validation
                 vloss = validate(model, val_data_loader)
                 if vloss < min_loss:
-                    torch.save(model.module.state_dict(),
-                               cam_weight_path + '.pth')
+                    torch.save(model.state_dict(), cam_weight_path + '.pth')
                     min_loss = vloss
                 timer.reset_stage()
         # empty cache
