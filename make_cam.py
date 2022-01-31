93a3312405a3365399d043852723b9748c02d407
Yiping Wang <yiping.wang@gmx.com>
2022-01-30 21:15:07 -0500

    .

--- make_cam.py
+++ make_cam.py
@@ -30,26 +30,26 @@ def _work(process_id, model, dataset, config):
             label = pack['label'][0]
             size = pack['size']
 
+            valid_cat = torch.nonzero(label)[:, 0]
+
             strided_size = imutils.get_strided_size(size, 4)
             strided_up_size = imutils.get_strided_up_size(size, 16)
 
             outputs = [model(img[0].cuda(non_blocking=True))
                        for img in pack['img']]
 
-            strided_cam = torch.sum(torch.stack(
-                [F.interpolate(torch.unsqueeze(o, 0), strided_size, mode='bilinear', align_corners=False)[0] for o
-                 in outputs]), 0)
-            strided_cam = strided_cam / \
-                (F.adaptive_max_pool2d(strided_cam, (1, 1)) + 1e-5)
+            strided_cam = torch.sum(torch.stack([F.interpolate(torch.unsqueeze(
+                o, 0), strided_size, mode='bilinear', align_corners=False)[0] for o in outputs]), 0)
+            strided_cam = strided_cam[valid_cat]
+            strided_cam /= F.adaptive_max_pool2d(strided_cam, (1, 1)) + 1e-5
 
-            highres_cam = [F.interpolate(torch.unsqueeze(o, 1), strided_up_size,
-                                         mode='bilinear', align_corners=False) for o in outputs]
+            highres_cam = [F.interpolate(torch.unsqueeze(
+                o, 1), strided_up_size, mode='bilinear', align_corners=False) for o in outputs]
             highres_cam = torch.sum(torch.stack(highres_cam, 0), 0)[
                 :, 0, :size[0], :size[1]]
-            highres_cam = highres_cam / \
-                (F.adaptive_max_pool2d(highres_cam, (1, 1)) + 1e-5)
-
-            valid_cat = torch.nonzero(label)[:, 0]
+                
+            highres_cam = highres_cam[valid_cat]
+            highres_cam /= F.adaptive_max_pool2d(highres_cam, (1, 1)) + 1e-5
 
             # save cams
             np.save(os.path.join(cam_out_dir, img_name + '.npy'),
