4ca4b6e37345415298edd2fe49c0f632056d2ed2
Yiping Wang <yiping.wang@gmx.com>
2022-02-02 14:04:29 -0500

    .

--- train_dual_front_door.py
+++ train_dual_front_door.py
@@ -51,7 +51,6 @@ def validate(cls_model, data_loader, logexpsum_r, cam_square_shape):
             labels = pack['label'].cuda(device, non_blocking=True)
             # P(z|x)
             x, scams = cls_model(imgs)
-            print(scams.shape)
             scams = F.interpolate(
                 scams, (cam_square_shape, cam_square_shape), mode='bilinear', align_corners=False)
             scams = scams / (F.adaptive_max_pool2d(scams, (1, 1)) + 1e-5)
@@ -135,6 +134,7 @@ def train(config, device):
             labels = pack['label'].cuda(device, non_blocking=True)
             # P(z|x)
             x, scams = cls_model(imgs)
+            print(scams.shape)
             scams = F.interpolate(
                 scams, (cam_square_shape, cam_square_shape), mode='bilinear', align_corners=False)
             scams = scams / (F.adaptive_max_pool2d(scams, (1, 1)) + 1e-5)
