c0dba4471430032fd8efc3d2ffcad192660c2ead
Yiping Wang <yiping.wang@gmx.com>
2022-02-26 14:21:56 -0500

    .

--- test.py
+++ test.py
@@ -2,41 +2,82 @@ import torch.nn.functional as F
 import voc12.dataloader
 import argparse
 import torch
-import glob
 import os
 import numpy as np
 from torch.utils.data import DataLoader
 from misc import pyutils, torchutils, imutils
-from net.resnet50_cam import CAMDualHeads, Net, CAM
-import itertools
-import operator
-import collections
+from net.resnet50_cam import Net, CAM
+from torch import nn
+from torchvision.transforms import transforms
+from voc12.dataloader import get_img_path
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+train_list = '/Users/Andy/Projects/front_door_cam/voc12/val.txt'
+voc12_root = '/Users/Andy/Projects/dataset/VOCdevkit/VOC2012'
+
+train_dataset = voc12.dataloader.VOC12ClassificationDataset(train_list, voc12_root=voc12_root,
+                                                            resize_long=(160, 320), hor_flip=True,
+                                                            crop_size=256, crop_method="random")
+train_data_loader = DataLoader(train_dataset,
+                               batch_size=2,
+                               shuffle=False,
+                               num_workers=1,
+                               pin_memory=True,
+                               drop_last=True)
+
+from net import resnet50
+
+class NetDualHeads(nn.Module):
+    def __init__(self):
+        super(NetDualHeads, self).__init__()
+
+        self.resnet50 = resnet50.resnet50(
+            pretrained=True, strides=(2, 2, 2, 1))
+        self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1, self.resnet50.relu, self.resnet50.maxpool,
+                                    self.resnet50.layer1)
+        self.stage2 = nn.Sequential(self.resnet50.layer2)
+        self.stage3 = nn.Sequential(self.resnet50.layer3)
+        self.stage4 = nn.Sequential(self.resnet50.layer4)
+
+        self.classifier = nn.Conv2d(2048, 20, 1, bias=False)
+
+        self.backbone = nn.ModuleList(
+            [self.stage1, self.stage2, self.stage3, self.stage4])
+        self.newly_added = nn.ModuleList([self.classifier])
+
+    def forward(self, x):
+        x = self.stage1(x)
+        x = self.stage2(x).detach()
+        x = self.stage3(x)
+        x = self.stage4(x)
+
+        logit = torchutils.gap2d(x, keepdims=True)
+        logit = self.classifier(logit)
+        logit = logit.view(-1, 20)
+
+        cam = F.conv2d(x, self.classifier.weight.detach())
+        cam = F.relu(cam)
+        cam /= F.adaptive_max_pool2d(cam, (1, 1)) + 1e-5
+        return cam, logit
+
+    def train(self, mode=True):
+        for p in self.resnet50.conv1.parameters():
+            p.requires_grad = False
+        for p in self.resnet50.bn1.parameters():
+            p.requires_grad = False
+
+    def trainable_parameters(self):
+        return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
 
 
-class IterateCAM:
-    def __init__(self, cam_dir):
-        self.cam_list = glob.glob(os.path.join(cam_dir, '*.npy'))
-        self.index = 0
+dm = NetDualHeads()
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index == len(self.cam_list):
-            raise StopIteration
-        c = np.load(self.cam_list[self.index], allow_pickle=True).item()
-        self.index += 1
-        return c['cam']
-
-
-def sum_cams(cam_dir):
-    itcam = IterateCAM(cam_dir)
-    running_sum = itertools.accumulate(itcam)
-    running_mean = map(operator.truediv, running_sum, itertools.count(1))
-    return collections.deque(running_mean, maxlen=1)[0]
-
-
-cam_out_dir = "/data/home/yipingwang/data/CAM"
-os.system('python3 make_cam.py')  # generate CAMs
-scams = sum_cams(cam_out_dir).cpu().numpy()
-np.save('scam.npy', scams)
+for pack in train_data_loader:
+    imgs = pack['img']
+    print(imgs.shape)
+    labels = pack['label']
+    cam, logit = dm(imgs)
+    print(cam.shape)
+    print(logit.shape)
---
+++ train_mix.py
@@ -0,0 +1,139 @@
+import voc12.dataloader
+import argparse
+import torch
+import os
+from torch.utils.data import DataLoader
+from misc import pyutils, torchutils
+from net.resnet50_cam import NetDualHeads
+
+
+def validate(cls_model, data_loader, device):
+    print('validating ... ', flush=True, end='')
+    val_loss_meter = pyutils.AverageMeter('loss', 'bce', 'cam')
+
+    cls_model.eval()
+
+    with torch.no_grad():
+        for pack in data_loader:
+            imgs = pack['img'].cuda(device, non_blocking=True)
+            labels = pack['label'].cuda(device, non_blocking=True)
+            logit, cam = cls_model(imgs)
+            bce_loss = torch.nn.BCEWithLogitsLoss()(logit, labels)
+            cam_loss = torch.nn.BCELoss()(torchutils.mean_agg(cam, r=1), labels)
+            loss = bce_loss + cam_loss
+            val_loss_meter.add(
+                {'loss': loss.item(), 'bce': bce_loss.item(), 'cam': cam_loss.item()})
+
+    cls_model.train()
+
+    loss = val_loss_meter.pop('loss')
+    bce = val_loss_meter.pop('bce')
+    cam = val_loss_meter.pop('cam')
+    print('Loss: {:.4f} | BCE: {:.4f} | CAM: {:.4f}'.format(loss, bce, cam))
+    return loss
+
+
+def train(config, device):
+    seed = config['seed']
+    train_list = config['train_list']
+    val_list = config['val_list']
+    voc12_root = config['voc12_root']
+    cam_batch_size = config['cam_batch_size']
+    cam_num_epoches = config['cam_num_epoches']
+    cam_learning_rate = config['cam_learning_rate']
+    cam_weight_decay = config['cam_weight_decay']
+    cam_crop_size = config['cam_crop_size']
+    model_root = config['model_root']
+    cam_weights_name = config['cam_weights_name']
+    num_workers = config['num_workers']
+    cam_weight_path = os.path.join(model_root, cam_weights_name)
+
+    pyutils.seed_all(seed)
+
+    # CAM generation dataset
+    train_dataset = voc12.dataloader.VOC12ClassificationDataset(train_list, voc12_root=voc12_root,
+                                                                resize_long=(160, 320), hor_flip=True,
+                                                                crop_size=cam_crop_size, crop_method="random")
+    train_data_loader = DataLoader(train_dataset,
+                                   batch_size=cam_batch_size,
+                                   shuffle=True,
+                                   num_workers=num_workers,
+                                   pin_memory=True,
+                                   drop_last=True)
+
+    max_step = (len(train_dataset) // cam_batch_size) * cam_num_epoches
+
+    val_dataset = voc12.dataloader.VOC12ClassificationDataset(val_list, voc12_root=voc12_root,
+                                                              crop_size=512)
+    val_data_loader = DataLoader(val_dataset,
+                                 batch_size=cam_batch_size,
+                                 shuffle=False,
+                                 num_workers=num_workers,
+                                 pin_memory=True,
+                                 drop_last=True)
+
+    cls_model = NetDualHeads().cuda(device)
+
+    param_groups = cls_model.trainable_parameters()
+    optimizer = torchutils.PolyOptimizer([
+        {'params': param_groups[0], 'lr': cam_learning_rate,
+            'weight_decay': cam_weight_decay},
+        {'params': param_groups[1], 'lr': 10 * cam_learning_rate,
+            'weight_decay': cam_weight_decay},
+    ], lr=cam_learning_rate, weight_decay=cam_weight_decay, max_step=max_step)
+
+    cls_model.train()
+
+    avg_meter = pyutils.AverageMeter('loss', 'bce', 'cam')
+    timer = pyutils.Timer()
+
+    min_loss = float('inf')
+
+    for ep in range(cam_num_epoches):
+        print('Epoch %d/%d' % (ep+1, cam_num_epoches))
+        for step, pack in enumerate(train_data_loader):
+            imgs = pack['img'].cuda(device, non_blocking=True)
+            labels = pack['label'].cuda(device, non_blocking=True)
+
+            logit, cam = cls_model(imgs)
+            bce_loss = torch.nn.BCEWithLogitsLoss()(logit, labels)
+            cam_loss = torch.nn.BCELoss()(torchutils.mean_agg(cam, r=1), labels)
+            loss = bce_loss + cam_loss
+            avg_meter.add(
+                {'loss': loss.item(), 'bce': bce_loss.item(), 'cam': cam_loss.item()})
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (optimizer.global_step - 1) % 100 == 0:
+                timer.update_progress(optimizer.global_step / max_step)
+                print('step:%5d/%5d' % (optimizer.global_step - 1, max_step),
+                      'Loss:%.4f' % (avg_meter.pop('loss')),
+                      'BCE:%.4f' % (avg_meter.pop('bce')),
+                      'CAM:%.4f' % (avg_meter.pop('cam')),
+                      'imps:%.1f' % (
+                          (step + 1) * cam_batch_size / timer.get_stage_elapsed()),
+                      'lr: %.4f' % (optimizer.param_groups[0]['lr']),
+                      'etc:%s' % (timer.str_estimated_complete()), flush=True)
+                # validation
+                vloss = validate(cls_model, val_data_loader, device)
+                if vloss < min_loss:
+                    torch.save(cls_model.state_dict(), cam_weight_path)
+                    min_loss = vloss
+
+                timer.reset_stage()
+        # empty cache
+        torch.cuda.empty_cache()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Front Door Semantic Segmentation')
+    parser.add_argument('--config', type=str,
+                        help='YAML config file path', default='./cfg/mix.yml')
+    args = parser.parse_args()
+    config = pyutils.parse_config(args.config)
+    device = torch.device('cuda:7')
+    train(config, device)
+    # os.system('python3 make_cam.py --config ./cfg/front_door.yml')
