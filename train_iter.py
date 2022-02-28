4dd06deda89bc2f32f7c6ad16b556133e74f26e1
Yiping Wang <yiping.wang@gmx.com>
2022-02-28 11:59:28 -0500

    .

--- train_iter.py
+++ train_iter.py
@@ -123,7 +123,7 @@ def train(config, device):
             'weight_decay': cam_weight_decay},
         {'params': param_groups[1], 'lr': 10 * cam_learning_rate,
             'weight_decay': cam_weight_decay},
-        {'params': mlp.parameters(), 'lr': 0.01 * cam_learning_rate,
+        {'params': mlp.parameters(), 'lr': 0.001 * cam_learning_rate,
             'weight_decay': cam_weight_decay},
     ], lr=cam_learning_rate, weight_decay=cam_weight_decay, max_step=max_step)
 
