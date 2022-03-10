204e20501f0e6ef1fa9dd297d412b7fd06944670
Yiping Wang <yiping.wang@gmx.com>
2022-03-10 09:55:45 -0500

    .

--- seg.sh
+++ seg.sh
@@ -1,8 +1,19 @@
-python3 make_cam_regular.py    --config ./cfg/baseline.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_baseline.yml
-python3 train_irn.py           --config ./cfg/ir_net_baseline.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_baseline.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_baseline.yml
+echo "======= Exp 1 ========="
+rm /data/home/yipingwang/data/CAMFdsi/*
+rm /data/home/yipingwang/data/IRLabelFdsi/*
+rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 make_cam_regular.py    --config ./cfg/fdsi_exp18.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_frontdoor.yml
+python3 train_irn.py           --config ./cfg/ir_net_frontdoor.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_frontdoor.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_frontdoor.yml
+echo "========================"
+
+# python3 make_cam_regular.py    --config ./cfg/baseline.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_baseline.yml
+# python3 train_irn.py           --config ./cfg/ir_net_baseline.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_baseline.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_baseline.yml
 
 
 # echo "======= Exp 1 ========="
