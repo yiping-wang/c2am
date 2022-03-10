c137f84c823ee1ffc946d7a4e54af2f7d7496eff
Yiping Wang <yiping.wang@gmx.com>
2022-03-10 12:16:27 -0500

    .

--- run_512.sh
+++ run_512.sh
@@ -1,3 +1,5 @@
+echo "======= Exp 18 ========="
+
 python3 train_fdsi.py --config ./cfg/fdsi_exp18.yml
 
 rm /data/home/yipingwang/data/CAMFdsi/*
@@ -9,6 +11,9 @@ python3 train_irn.py           --config ./cfg/ir_net_exp18.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp18.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp18.yml
 
+echo "========================"
+
+echo "======= Exp 19 ========="
 
 python3 train_fdsi.py --config ./cfg/fdsi_exp19.yml
 
@@ -21,6 +26,10 @@ python3 train_irn.py           --config ./cfg/ir_net_exp19.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp19.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp19.yml
 
+echo "========================"
+
+
+echo "======= Exp 20 ========="
 
 python3 train_fdsi.py --config ./cfg/fdsi_exp20.yml
 
@@ -33,6 +42,9 @@ python3 train_irn.py           --config ./cfg/ir_net_exp20.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp20.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp20.yml
 
+echo "========================"
+
+echo "======= Exp 21 ========="
 
 python3 train_fdsi.py --config ./cfg/fdsi_exp21.yml
 
@@ -44,3 +56,5 @@ python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp21.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp21.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp21.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp21.yml
+
+echo "========================"
