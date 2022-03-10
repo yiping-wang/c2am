cde3a7648550f256a0770033773936bf65d088eb
Yiping Wang <yiping.wang@gmx.com>
2022-03-10 13:10:43 -0500

    .

--- run_256.sh
+++ run_256.sh
@@ -1,9 +1,9 @@
-echo "================== Baseline E10 ==========================="
+echo "================== Baseline ==========================="
 
 rm /data/home/yipingwang/data/CAMBaseline/*
 rm /data/home/yipingwang/data/IRLabelBaseline/*
 rm /data/home/yipingwang/data/SemSegBaseline/*
-python3 make_cam_regular.py    --config ./cfg/baseline.yml
+python3 make_cam_regular.py    --config ./cfg/baseline_256.yml
 python3 cam_to_ir_label.py     --config ./cfg/ir_net_baseline_256.yml
 python3 train_irn.py           --config ./cfg/ir_net_baseline_256.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_baseline_256.yml
@@ -195,3 +195,17 @@ python3 train_irn.py           --config ./cfg/ir_net_exp14.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp14.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp14.yml
 echo "==========================================================" 
+
+
+echo "================== Baseline E10 ==========================="
+
+rm /data/home/yipingwang/data/CAMBaseline/*
+rm /data/home/yipingwang/data/IRLabelBaseline/*
+rm /data/home/yipingwang/data/SemSegBaseline/*
+python3 make_cam_regular.py    --config ./cfg/baseline_256_e10.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_baseline_256_e10.yml
+python3 train_irn.py           --config ./cfg/ir_net_baseline_256_e10.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_baseline_256_e10.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_baseline_256_e10.yml
+
+echo "=========================================================="
