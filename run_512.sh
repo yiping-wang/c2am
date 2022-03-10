dcd0693e50896461a37deb4a1bed78eed096236a
Yiping Wang <yiping.wang@gmx.com>
2022-03-10 12:55:36 -0500

    .

--- run_512.sh
+++ run_512.sh
@@ -1,7 +1,5 @@
 echo "======= Exp 18 ========="
-
 python3 train_fdsi.py --config ./cfg/fdsi_exp18.yml
-
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
@@ -10,13 +8,10 @@ python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp18.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp18.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp18.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp18.yml
-
 echo "========================"
 
 echo "======= Exp 19 ========="
-
 python3 train_fdsi.py --config ./cfg/fdsi_exp19.yml
-
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
@@ -25,14 +20,11 @@ python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp19.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp19.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp19.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp19.yml
-
 echo "========================"
 
 
 echo "======= Exp 20 ========="
-
 python3 train_fdsi.py --config ./cfg/fdsi_exp20.yml
-
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
@@ -41,13 +33,10 @@ python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp20.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp20.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp20.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp20.yml
-
 echo "========================"
 
 echo "======= Exp 21 ========="
-
 python3 train_fdsi.py --config ./cfg/fdsi_exp21.yml
-
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
@@ -56,5 +45,4 @@ python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp21.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp21.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp21.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_exp21.yml
-
 echo "========================"
