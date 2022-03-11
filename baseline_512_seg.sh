38ea7ab1d87fa2552c8dc52d27c60ba74a494263
Yiping Wang <yiping.wang@gmx.com>
2022-03-11 12:28:39 -0500

    .

--- baseline_512_seg.sh
+++
@@ -1,9 +0,0 @@
-rm /data/home/yipingwang/data/CAMBaseline/*
-rm /data/home/yipingwang/data/IRLabelBaseline/*
-rm /data/home/yipingwang/data/SemSegBaseline/*
-
-python3 make_cam_regular.py    --config ./cfg/baseline_512.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_baseline_512.yml
-python3 train_irn.py           --config ./cfg/ir_net_baseline_512.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_baseline_512.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_baseline_512.yml
