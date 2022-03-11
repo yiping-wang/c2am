f29fd289cb6386f569d1544e5a707a4b58ef3090
Yiping Wang <yiping.wang@gmx.com>
2022-03-10 22:00:01 -0500

    .

--- run_512.sh
+++ run_512.sh
@@ -1,8 +1,8 @@
 echo "======= Exp 18 ========="
-python3 train_fdsi.py --config ./cfg/fdsi_exp18.yml
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 train_fdsi.py --config ./cfg/fdsi_exp18.yml
 python3 make_cam_regular.py    --config ./cfg/fdsi_exp18.yml
 python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp18.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp18.yml
@@ -11,10 +11,10 @@ python3 eval_sem_seg.py        --config ./cfg/ir_net_exp18.yml
 echo "========================"
 
 echo "======= Exp 19 ========="
-python3 train_fdsi.py --config ./cfg/fdsi_exp19.yml
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 train_fdsi.py --config ./cfg/fdsi_exp19.yml
 python3 make_cam_regular.py    --config ./cfg/fdsi_exp19.yml
 python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp19.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp19.yml
@@ -24,10 +24,10 @@ echo "========================"
 
 
 echo "======= Exp 20 ========="
-python3 train_fdsi.py --config ./cfg/fdsi_exp20.yml
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 train_fdsi.py --config ./cfg/fdsi_exp20.yml
 python3 make_cam_regular.py    --config ./cfg/fdsi_exp20.yml
 python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp20.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp20.yml
@@ -36,10 +36,10 @@ python3 eval_sem_seg.py        --config ./cfg/ir_net_exp20.yml
 echo "========================"
 
 echo "======= Exp 21 ========="
-python3 train_fdsi.py --config ./cfg/fdsi_exp21.yml
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 train_fdsi.py --config ./cfg/fdsi_exp21.yml
 python3 make_cam_regular.py    --config ./cfg/fdsi_exp21.yml
 python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp21.yml
 python3 train_irn.py           --config ./cfg/ir_net_exp21.yml
