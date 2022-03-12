130fa38221e0bb4f5c8023f56d104917c0ce094b
Yiping Wang <yiping.wang@gmx.com>
2022-03-12 14:06:15 -0500

    .

--- run_256.sh
+++ run_256.sh
@@ -11,9 +11,9 @@
 
 
 echo "================== Exp 15 ================================="
-rm /data/home/yipingwang/data/CAMFdsiExp15/*
-rm /data/home/yipingwang/data/IRLabelFdsiExp15/*
-rm /data/home/yipingwang/data/SemSegFdsiExp15/*
+rm /data/home/yipingwang/data/CAMFdsi/*
+rm /data/home/yipingwang/data/IRLabelFdsi/*
+rm /data/home/yipingwang/data/SemSegFdsi/*
 # python3 train_fdsi.py --config ./cfg/fdsi_exp15.yml
 python3 make_cam_regular.py    --config ./cfg/fdsi_exp15.yml
 python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp15.yml
