e7aac6c2cc3cd46a8f19111612667f707a2bea2e
Yiping Wang <yiping.wang@gmx.com>
2022-03-13 15:24:53 -0400

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -1,7 +1,9 @@
 import torch.nn as nn
+import torch
 import torch.nn.functional as F
-from misc import torchutils
+from misc import torchutils, pyutils
 from net import resnet50
+import os
 
 
 class MLP(nn.Module):
@@ -86,6 +88,56 @@ class CAM(nn.Module):
         x = F.conv2d(x, self.classifier.weight)
         x = F.relu(x)
         x = x[0] + x[1].flip(-1)
+        return x
+
+    def train(self, mode=True):
+        for p in self.resnet50.conv1.parameters():
+            p.requires_grad = False
+        for p in self.resnet50.bn1.parameters():
+            p.requires_grad = False
+
+    def trainable_parameters(self):
+        return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
+
+
+class IntervenedCAM(nn.Module):
+    def __init__(self, cam_dir):
+        super(IntervenedCAM, self).__init__()
+        self.resnet50 = resnet50.resnet50(
+            pretrained=True, strides=(2, 2, 2, 1))
+
+        self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1, self.resnet50.relu, self.resnet50.maxpool,
+                                    self.resnet50.layer1)
+        self.stage2 = nn.Sequential(self.resnet50.layer2)
+        self.stage3 = nn.Sequential(self.resnet50.layer3)
+        self.stage4 = nn.Sequential(self.resnet50.layer4)
+
+        self.classifier = nn.Conv2d(2048, 20, 1, bias=False)
+
+        self.backbone = nn.ModuleList(
+            [self.stage1, self.stage2, self.stage3, self.stage4])
+        self.newly_added = nn.ModuleList([self.classifier])
+
+        self.cam_is_empty = len(os.listdir(cam_dir)) == 0
+        self.cam_dir = cam_dir
+
+    def forward(self, x):
+        if self.cam_is_empty:
+            scams = torch.ones(20, 32, 32)
+        else:
+            scams = pyutils.sum_cams(self.cam_dir).cuda(non_blocking=True)
+
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+
+        x = F.conv2d(x, self.classifier.weight)
+        x = x * scams.unsqueeze(0)
+
+        x = F.relu(x)
+        x = x[0] + x[1].flip(-1)
+
         return x
 
     def train(self, mode=True):
--- run_512.sh
+++ run_512.sh
@@ -1,52 +1,39 @@
-echo "======= Exp 30 ========="
+echo "======= Exp E ========="
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp30.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp30.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp30.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp30.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp30.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp30.yml
+python3 train_fdsi.py --config ./cfg/fdsi_expE.yml
+python3 make_intervened_cam.py --config ./cfg/fdsi_expE.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_expE.yml
+python3 train_irn.py           --config ./cfg/ir_net_expE.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_expE.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_expE.yml
 echo "========================"
 
-echo "======= Exp 31 ========="
-rm /data/home/yipingwang/data/CAMFdsi/*
-rm /data/home/yipingwang/data/IRLabelFdsi/*
-rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp31.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp31.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp31.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp31.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp31.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp31.yml
-echo "========================"
+# echo "======= Exp 32 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp32.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp32.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp32.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp32.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp32.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp32.yml
+# echo "========================"
 
 
-echo "======= Exp 32 ========="
-rm /data/home/yipingwang/data/CAMFdsi/*
-rm /data/home/yipingwang/data/IRLabelFdsi/*
-rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp32.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp32.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp32.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp32.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp32.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp32.yml
-echo "========================"
-
-
-echo "======= Exp 33 ========="
-rm /data/home/yipingwang/data/CAMFdsi/*
-rm /data/home/yipingwang/data/IRLabelFdsi/*
-rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp33.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp33.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp33.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp33.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp33.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp33.yml
-echo "========================"
+# echo "======= Exp 33 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp33.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp33.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp33.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp33.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp33.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp33.yml
+# echo "========================"
 
 
 # echo "======= Exp 25 ========="
@@ -196,4 +183,28 @@ echo "========================"
 # python3 train_irn.py           --config ./cfg/ir_net_exp24.yml
 # python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp24.yml
 # python3 eval_sem_seg.py        --config ./cfg/ir_net_exp24.yml
+# echo "========================"
+
+# echo "======= Exp 30 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp30.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp30.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp30.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp30.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp30.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp30.yml
+# echo "========================"
+
+# echo "======= Exp 31 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp31.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp31.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp31.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp31.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp31.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp31.yml
 # echo "========================"
