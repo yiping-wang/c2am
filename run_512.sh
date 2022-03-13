a4434dbfaae63f294cbc71aae5a25bd9350fd718
Yiping Wang <yiping.wang@gmx.com>
2022-03-13 11:04:11 -0400

    .

--- run_512.sh
+++ run_512.sh
@@ -1,3 +1,28 @@
+echo "======= Exp 30 ========="
+rm /data/home/yipingwang/data/CAMFdsi/*
+rm /data/home/yipingwang/data/IRLabelFdsi/*
+rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 train_fdsi.py --config ./cfg/fdsi_exp30.yml
+python3 make_cam_regular.py    --config ./cfg/fdsi_exp30.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp30.yml
+python3 train_irn.py           --config ./cfg/ir_net_exp30.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp30.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_exp30.yml
+echo "========================"
+
+echo "======= Exp 31 ========="
+rm /data/home/yipingwang/data/CAMFdsi/*
+rm /data/home/yipingwang/data/IRLabelFdsi/*
+rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 train_fdsi.py --config ./cfg/fdsi_exp31.yml
+python3 make_cam_regular.py    --config ./cfg/fdsi_exp31.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp31.yml
+python3 train_irn.py           --config ./cfg/ir_net_exp31.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp31.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_exp31.yml
+echo "========================"
+
+
 # echo "======= Exp 25 ========="
 # rm /data/home/yipingwang/data/CAMFdsi/*
 # rm /data/home/yipingwang/data/IRLabelFdsi/*
@@ -11,53 +36,53 @@
 # echo "========================"
 
 
-echo "======= Exp 26 ========="
-rm /data/home/yipingwang/data/CAMFdsi/*
-rm /data/home/yipingwang/data/IRLabelFdsi/*
-rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp26.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp26.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp26.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp26.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp26.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp26.yml
-echo "========================"
+# echo "======= Exp 26 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp26.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp26.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp26.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp26.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp26.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp26.yml
+# echo "========================"
 
-echo "======= Exp 27 ========="
-rm /data/home/yipingwang/data/CAMFdsi/*
-rm /data/home/yipingwang/data/IRLabelFdsi/*
-rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp27.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp27.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp27.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp27.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp27.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp27.yml
-echo "========================"
+# echo "======= Exp 27 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp27.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp27.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp27.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp27.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp27.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp27.yml
+# echo "========================"
 
-echo "======= Exp 28 ========="
-rm /data/home/yipingwang/data/CAMFdsi/*
-rm /data/home/yipingwang/data/IRLabelFdsi/*
-rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp28.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp28.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp28.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp28.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp28.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp28.yml
-echo "========================"
+# echo "======= Exp 28 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp28.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp28.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp28.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp28.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp28.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp28.yml
+# echo "========================"
 
-echo "======= Exp 29 ========="
-rm /data/home/yipingwang/data/CAMFdsi/*
-rm /data/home/yipingwang/data/IRLabelFdsi/*
-rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp29.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp29.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp29.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp29.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp29.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp29.yml
-echo "========================"
+# echo "======= Exp 29 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp29.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp29.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp29.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp29.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp29.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp29.yml
+# echo "========================"
 
 
 # echo "======= Exp 19 ========="
