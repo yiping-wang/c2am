ae4cf560db9a830a8688f8a914145c05bf9de030
Yiping Wang <yiping.wang@gmx.com>
2022-03-13 13:30:04 -0400

    .

--- run_512.sh
+++ run_512.sh
@@ -36,6 +36,19 @@ python3 eval_sem_seg.py        --config ./cfg/ir_net_exp32.yml
 echo "========================"
 
 
+echo "======= Exp 33 ========="
+rm /data/home/yipingwang/data/CAMFdsi/*
+rm /data/home/yipingwang/data/IRLabelFdsi/*
+rm /data/home/yipingwang/data/SemSegFdsi/*
+python3 train_fdsi.py --config ./cfg/fdsi_exp33.yml
+python3 make_cam_regular.py    --config ./cfg/fdsi_exp33.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp33.yml
+python3 train_irn.py           --config ./cfg/ir_net_exp33.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp33.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_exp33.yml
+echo "========================"
+
+
 # echo "======= Exp 25 ========="
 # rm /data/home/yipingwang/data/CAMFdsi/*
 # rm /data/home/yipingwang/data/IRLabelFdsi/*
