45f0a7f8360701d213c5143dcd463e8e219b3194
Yiping Wang <yiping.wang@gmx.com>
2022-03-14 01:28:00 -0400

    .

--- run_512.sh
+++ run_512.sh
@@ -1,15 +1,14 @@
-echo "======= Exp E ========="
+echo "======= Exp 35 ========="
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_intervened_fdsi.py --config ./cfg/fdsi_expE.yml
-python3 make_cam_regular.py      --config ./cfg/fdsi_expE.yml
-python3 cam_to_ir_label.py       --config ./cfg/ir_net_expE.yml
-python3 train_irn.py             --config ./cfg/ir_net_expE.yml
-python3 make_sem_seg_labels.py   --config ./cfg/ir_net_expE.yml
-python3 eval_sem_seg.py          --config ./cfg/ir_net_expE.yml
+python3 train_fdsi.py --config ./cfg/fdsi_exp35.yml
+python3 make_cam_regular.py    --config ./cfg/fdsi_exp35.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp35.yml
+python3 train_irn.py           --config ./cfg/ir_net_exp35.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp35.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_exp35.yml
 echo "========================"
-
 
 # echo "======= Exp 15 ========="
 # rm /data/home/yipingwang/data/CAMFdsi/*
