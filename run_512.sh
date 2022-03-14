3c57bc6cce1abc0eb04c808408e4ad63ab0b557f
Yiping Wang <yiping.wang@gmx.com>
2022-03-14 00:01:53 -0400

    .

--- run_512.sh
+++ run_512.sh
@@ -1,13 +1,26 @@
-echo "======= Exp 15 ========="
+echo "======= Exp E ========="
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp15.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp15.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp15.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp15.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp15.yml
+python3 train_intervened_fdsi.py --config ./cfg/fdsi_expE.yml
+python3 make_cam_regular.py      --config ./cfg/fdsi_expE.yml
+python3 cam_to_ir_label.py       --config ./cfg/ir_net_expE.yml
+python3 train_irn.py             --config ./cfg/ir_net_expE.yml
+python3 make_sem_seg_labels.py   --config ./cfg/ir_net_expE.yml
+python3 eval_sem_seg.py          --config ./cfg/ir_net_expE.yml
 echo "========================"
+
+
+# echo "======= Exp 15 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp15.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp15.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp15.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp15.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp15.yml
+# echo "========================"
 
 
 # echo "======= Exp 32 ========="
@@ -207,16 +220,4 @@ echo "========================"
 # python3 train_irn.py           --config ./cfg/ir_net_exp31.yml
 # python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp31.yml
 # python3 eval_sem_seg.py        --config ./cfg/ir_net_exp31.yml
-# echo "========================"
-
-# echo "======= Exp E ========="
-# rm /data/home/yipingwang/data/CAMFdsi/*
-# rm /data/home/yipingwang/data/IRLabelFdsi/*
-# rm /data/home/yipingwang/data/SemSegFdsi/*
-# python3 train_fdsi.py --config ./cfg/fdsi_expE.yml
-# python3 make_intervened_cam.py --config ./cfg/fdsi_expE.yml
-# python3 cam_to_ir_label.py     --config ./cfg/ir_net_expE.yml
-# python3 train_irn.py           --config ./cfg/ir_net_expE.yml
-# python3 make_sem_seg_labels.py --config ./cfg/ir_net_expE.yml
-# python3 eval_sem_seg.py        --config ./cfg/ir_net_expE.yml
 # echo "========================"
