ab72a009c9bd2ab2f01649fc14914783e0110e7d
Yiping Wang <yiping.wang@gmx.com>
2022-03-14 23:23:22 -0400

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -188,3 +188,65 @@ class IntervenedCAM(nn.Module):
 
     def trainable_parameters(self):
         return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
+
+
+class Net_CAM_Feature(Net):
+
+    def __init__(self, stride=16, n_classes=20):
+        super(Net_CAM_Feature, self).__init__(
+            stride=stride, n_classes=n_classes)
+
+    def forward(self, x):
+
+        x = self.stage1(x)
+        x = self.stage2(x)
+
+        x = self.stage3(x)
+        feature = self.stage4(x)  # bs*2048*32*32
+
+        x = torchutils.gap2d(feature, keepdims=True)
+        x = self.classifier(x)
+        x = x.view(-1, self.n_classes)
+
+        cams = F.conv2d(feature, self.classifier.weight)
+        cams = F.relu(cams)
+        cams = cams/(F.adaptive_max_pool2d(cams, (1, 1)) + 1e-5)
+        cams_feature = cams.unsqueeze(
+            2)*feature.unsqueeze(1)  # bs*20*2048*32*32
+        cams_feature = cams_feature.view(cams_feature.size(
+            0), cams_feature.size(1), cams_feature.size(2), -1)
+        cams_feature = torch.mean(cams_feature, -1)
+
+        return x, cams_feature, cams
+
+
+class Class_Predictor(nn.Module):
+    def __init__(self, num_classes, representation_size):
+        super(Class_Predictor, self).__init__()
+        self.num_classes = num_classes
+        self.classifier = nn.Conv2d(
+            representation_size, num_classes, 1, bias=False)
+
+    def forward(self, x, label):
+        batch_size = x.shape[0]
+        x = x.reshape(batch_size, self.num_classes, -1)  # bs*20*2048
+        mask = label > 0  # bs*20
+
+        feature_list = [x[i][mask[i]] for i in range(batch_size)]  # bs*n*2048
+        prediction = [self.classifier(
+            y.unsqueeze(-1).unsqueeze(-1)).squeeze(-1).squeeze(-1) for y in feature_list]
+        labels = [torch.nonzero(label[i]).squeeze(1)
+                  for i in range(label.shape[0])]
+
+        loss = 0
+        acc = 0
+        num = 0
+        for logit, label in zip(prediction, labels):
+            if label.shape[0] == 0:
+                continue
+            loss_ce = F.cross_entropy(logit, label)
+            loss += loss_ce
+            acc += (logit.argmax(dim=1) == label.view(-1)).sum().float()
+            num += label.size(0)
+
+        return loss/batch_size, acc/num
--- run_512.sh
+++ run_512.sh
@@ -1,14 +1,26 @@
-echo "======= Exp 36 ========="
+echo "======= Exp E ========="
 rm /data/home/yipingwang/data/CAMFdsi/*
 rm /data/home/yipingwang/data/IRLabelFdsi/*
 rm /data/home/yipingwang/data/SemSegFdsi/*
-python3 train_fdsi.py --config ./cfg/fdsi_exp36.yml
-python3 make_cam_regular.py    --config ./cfg/fdsi_exp36.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp36.yml
-python3 train_irn.py           --config ./cfg/ir_net_exp36.yml
-python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp36.yml
-python3 eval_sem_seg.py        --config ./cfg/ir_net_exp36.yml
+python3 train_fdsi.py --config ./cfg/fdsi_expE.yml
+python3 make_cam_regular.py    --config ./cfg/fdsi_expE.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_expE.yml
+python3 train_irn.py           --config ./cfg/ir_net_expE.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_expE.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_expE.yml
 echo "========================"
+
+# echo "======= Exp 36 ========="
+# rm /data/home/yipingwang/data/CAMFdsi/*
+# rm /data/home/yipingwang/data/IRLabelFdsi/*
+# rm /data/home/yipingwang/data/SemSegFdsi/*
+# python3 train_fdsi.py --config ./cfg/fdsi_exp36.yml
+# python3 make_cam_regular.py    --config ./cfg/fdsi_exp36.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_exp36.yml
+# python3 train_irn.py           --config ./cfg/ir_net_exp36.yml
+# python3 make_sem_seg_labels.py --config ./cfg/ir_net_exp36.yml
+# python3 eval_sem_seg.py        --config ./cfg/ir_net_exp36.yml
+# echo "========================"
 
 # echo "======= Exp 35 ========="
 # rm /data/home/yipingwang/data/CAMFdsi/*
