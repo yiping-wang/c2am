cc993c34498d91454342660004e5b8af1a9e0b11
Yiping Wang <yiping.wang@gmx.com>
2022-03-14 23:27:54 -0400

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -190,7 +190,57 @@ class IntervenedCAM(nn.Module):
         return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
 
 
-class Net_CAM_Feature(Net):
+class ReNet(nn.Module):
+
+    def __init__(self, stride=16, n_classes=20):
+        super(ReNet, self).__init__()
+        if stride == 16:
+            self.resnet50 = resnet50.resnet50(
+                pretrained=True, strides=(2, 2, 2, 1))
+            self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1,
+                                        self.resnet50.relu, self.resnet50.maxpool, self.resnet50.layer1)
+        else:
+            self.resnet50 = resnet50.resnet50(
+                pretrained=True, strides=(2, 2, 1, 1), dilations=(1, 1, 2, 2))
+            self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1,
+                                        self.resnet50.relu, self.resnet50.maxpool, self.resnet50.layer1)
+        self.stage2 = nn.Sequential(self.resnet50.layer2)
+        self.stage3 = nn.Sequential(self.resnet50.layer3)
+        self.stage4 = nn.Sequential(self.resnet50.layer4)
+        self.n_classes = n_classes
+        self.classifier = nn.Conv2d(2048, n_classes, 1, bias=False)
+
+        self.backbone = nn.ModuleList(
+            [self.stage1, self.stage2, self.stage3, self.stage4])
+        self.newly_added = nn.ModuleList([self.classifier])
+
+    def forward(self, x):
+
+        x = self.stage1(x)
+        x = self.stage2(x)
+
+        x = self.stage3(x)
+        x = self.stage4(x)
+
+        x = torchutils.gap2d(x, keepdims=True)
+        x = self.classifier(x)
+        x = x.view(-1, self.n_classes)
+
+        return x
+
+    def train(self, mode=True):
+        super(Net, self).train(mode)
+        for p in self.resnet50.conv1.parameters():
+            p.requires_grad = False
+        for p in self.resnet50.bn1.parameters():
+            p.requires_grad = False
+
+    def trainable_parameters(self):
+
+        return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
+
+
+class Net_CAM_Feature(ReNet):
 
     def __init__(self, stride=16, n_classes=20):
         super(Net_CAM_Feature, self).__init__(
