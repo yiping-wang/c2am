a7ca94a2d23a457bcd2667b541f0a2625aa1d413
Yiping Wang <yiping.wang@gmx.com>
2022-03-15 16:52:46 -0400

    .

--- run_recam.sh
+++ run_recam.sh
@@ -6,8 +6,8 @@ echo "======= Exp ReCAM ========="
 # rm /data/home/yipingwang/data/IRLabelReCAM/*
 # rm /data/home/yipingwang/data/SemSegReCAM/*
 # python3 train_recam_fdsi.py --config ./cfg/fdsi_expReCAM.yml
-python3 make_recam_regular.py    --config ./cfg/fdsi_expReCAM.yml
-python3 cam_to_ir_label.py     --config ./cfg/ir_net_expReCAM.yml
+# python3 make_recam_regular.py    --config ./cfg/fdsi_expReCAM.yml
+# python3 cam_to_ir_label.py     --config ./cfg/ir_net_expReCAM.yml
 python3 train_irn.py           --config ./cfg/ir_net_expReCAM.yml
 python3 make_sem_seg_labels.py --config ./cfg/ir_net_expReCAM.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_expReCAM.yml
