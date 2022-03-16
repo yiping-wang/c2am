e42d23a7b16238e4b87f98bbf7acab39a74e1ed7
Yiping Wang <yiping.wang@gmx.com>
2022-03-15 22:27:27 -0400

    .

--- train_recam_fdsi.py
+++ train_recam_fdsi.py
@@ -179,7 +179,7 @@ def train(config, config_path):
             # agg(P(z|x) * sum(P(y|x, z) * P(x)))
             x = torchutils.mean_agg(x, r=agg_smooth_r)
             # Entropy loss for Content Adjustment
-            bce_loss = torch.nn.BCEWithLogitsLoss(x, labels)
+            bce_loss = torch.nn.BCEWithLogitsLoss()(x, labels)
             sce_loss, _ = recam_predictor(cam, labels)
             kl_loss = torch.tensor(0.).cuda(
             ) if alpha > 0 else torch.tensor(0.)
