0013c909df093537e8c85ff64414ee96997d69ab
Yiping Wang <yiping.wang@gmx.com>
2022-03-17 09:45:54 -0400

    .

--- run_fdsi.sh
+++ run_fdsi.sh
@@ -7,26 +7,26 @@ mkdir -p /data/home/yipingwang/data/SemSegFdsi/
 # rm /data/home/yipingwang/data/SemSegFdsi/*
 # python3 train_fdsi_datasetwise.py --config    ./cfg/fdsi_datasetwise_01.yml
 python3 make_cam_regular.py       --config    ./cfg/fdsi_datasetwise_01.yml
-python3 cam_to_ir_label.py        --config    ./cfg/ir_net_datasetwise_01.yml
-python3 train_irn.py              --config    ./cfg/ir_net_datasetwise_01.yml
-python3 make_sem_seg_labels.py    --config    ./cfg/ir_net_datasetwise_01.yml
-python3 eval_sem_seg.py           --config    ./cfg/ir_net_datasetwise_01.yml
+python3 cam_to_ir_label.py        --config    ./cfg/ir_net_datasetwise_02.yml
+python3 train_irn.py              --config    ./cfg/ir_net_datasetwise_02.yml
+python3 make_sem_seg_labels.py    --config    ./cfg/ir_net_datasetwise_02.yml
+python3 eval_sem_seg.py           --config    ./cfg/ir_net_datasetwise_02.yml
 echo "======================================="
 
-echo "======= Exp ReCAM ====================="
-mkdir -p /data/home/yipingwang/data/CAMReCAM/
-mkdir -p /data/home/yipingwang/data/IRLabelReCAM/
-mkdir -p /data/home/yipingwang/data/SemSegReCAM/
-rm /data/home/yipingwang/data/CAMReCAM/*
-rm /data/home/yipingwang/data/IRLabelReCAM/*
-rm /data/home/yipingwang/data/SemSegReCAM/*
-python3 train_recam_fdsi.py      --config ./cfg/fdsi_expReCAM.yml
-python3 make_recam_regular.py    --config ./cfg/fdsi_expReCAM.yml
-python3 cam_to_ir_label.py       --config ./cfg/ir_net_expReCAM.yml
-python3 train_irn.py             --config ./cfg/ir_net_expReCAM.yml
-python3 make_sem_seg_labels.py   --config ./cfg/ir_net_expReCAM.yml
-python3 eval_sem_seg.py          --config ./cfg/ir_net_expReCAM.yml
-echo "======================================="
+# echo "======= Exp ReCAM ====================="
+# mkdir -p /data/home/yipingwang/data/CAMReCAM/
+# mkdir -p /data/home/yipingwang/data/IRLabelReCAM/
+# mkdir -p /data/home/yipingwang/data/SemSegReCAM/
+# rm /data/home/yipingwang/data/CAMReCAM/*
+# rm /data/home/yipingwang/data/IRLabelReCAM/*
+# rm /data/home/yipingwang/data/SemSegReCAM/*
+# python3 train_recam_fdsi.py      --config ./cfg/fdsi_expReCAM.yml
+# python3 make_recam_regular.py    --config ./cfg/fdsi_expReCAM.yml
+# python3 cam_to_ir_label.py       --config ./cfg/ir_net_expReCAM.yml
+# python3 train_irn.py             --config ./cfg/ir_net_expReCAM.yml
+# python3 make_sem_seg_labels.py   --config ./cfg/ir_net_expReCAM.yml
+# python3 eval_sem_seg.py          --config ./cfg/ir_net_expReCAM.yml
+# echo "======================================="
 
 # echo "======= Exp FDSI Batch-wise ========="
 # mkdir -p /data/home/yipingwang/data/CAMFdsi/
