9cc2b192848a06b68ccb34dd29aede3f1bf7609b
Yiping Wang <yiping.wang@gmx.com>
2022-03-17 22:56:01 -0400

    .

--- run_recam.sh
+++ run_recam.sh
@@ -13,17 +13,17 @@ python3 make_sem_seg_labels.py --config ./cfg/ir_net_expReCAM_fd.yml
 python3 eval_sem_seg.py        --config ./cfg/ir_net_expReCAM_fd.yml
 echo "========================"
 
-# echo "======= Exp ReCAM ========="
-# mkdir -p /data/home/yipingwang/data/CAMReCAM/
-# mkdir -p /data/home/yipingwang/data/IRLabelReCAM/
-# mkdir -p /data/home/yipingwang/data/SemSegReCAM/
-# rm /data/home/yipingwang/data/CAMReCAM/*
-# rm /data/home/yipingwang/data/IRLabelReCAM/*
-# rm /data/home/yipingwang/data/SemSegReCAM/*
+echo "======= Exp ReCAM ========="
+mkdir -p /data/home/yipingwang/data/CAMReCAM/
+mkdir -p /data/home/yipingwang/data/IRLabelReCAM/
+mkdir -p /data/home/yipingwang/data/SemSegReCAM/
+rm /data/home/yipingwang/data/CAMReCAM/*
+rm /data/home/yipingwang/data/IRLabelReCAM/*
+rm /data/home/yipingwang/data/SemSegReCAM/*
 # python3 train_recam_fdsi.py --config ./cfg/fdsi_expReCAM.yml
-# python3 make_recam_regular.py    --config ./cfg/fdsi_expReCAM.yml
-# python3 cam_to_ir_label.py     --config ./cfg/ir_net_expReCAM.yml
-# python3 train_irn.py           --config ./cfg/ir_net_expReCAM.yml
-# python3 make_sem_seg_labels.py --config ./cfg/ir_net_expReCAM.yml
-# python3 eval_sem_seg.py        --config ./cfg/ir_net_expReCAM.yml
-# echo "========================"
+python3 make_recam_regular.py    --config ./cfg/fdsi_expReCAM.yml
+python3 cam_to_ir_label.py     --config ./cfg/ir_net_expReCAM_defaultparams.yml
+python3 train_irn.py           --config ./cfg/ir_net_expReCAM_defaultparams.yml
+python3 make_sem_seg_labels.py --config ./cfg/ir_net_expReCAM_defaultparams.yml
+python3 eval_sem_seg.py        --config ./cfg/ir_net_expReCAM_defaultparams.yml
+echo "========================"
