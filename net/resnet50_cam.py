aa2faf2b047cea30a0b737dfbe7d760d3e34428a
Yiping Wang <yiping.wang@gmx.com>
2022-03-21 14:42:15 -0400

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -22,47 +22,6 @@ class MLP(nn.Module):
         return self.projection_head(x)
 
 
-class IntervenedNet(nn.Module):
-    def __init__(self):
-        super(IntervenedNet, self).__init__()
-
-        self.resnet50 = resnet50.resnet50(
-            pretrained=True, strides=(2, 2, 2, 1))
-
-        self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1, self.resnet50.relu, self.resnet50.maxpool,
-                                    self.resnet50.layer1)
-        self.stage2 = nn.Sequential(self.resnet50.layer2)
-        self.stage3 = nn.Sequential(self.resnet50.layer3)
-        self.stage4 = nn.Sequential(self.resnet50.layer4)
-
-        self.classifier = nn.Conv2d(2048, 20, 1, bias=False)
-
-        self.backbone = nn.ModuleList(
-            [self.stage1, self.stage2, self.stage3, self.stage4])
-        self.newly_added = nn.ModuleList([self.classifier])
-
-    def forward(self, x, scams):
-        x = self.stage1(x)
-        x = self.stage2(x)
-        x = self.stage3(x)
-        x = self.stage4(x)
-        x = x * scams.unsqueeze(0)
-
-        feat = torchutils.gap2d(x, keepdims=True)
-        x = self.classifier(feat)
-        x = x.view(-1, 20)
-        return x, feat.squeeze()
-
-    def train(self, mode=True):
-        for p in self.resnet50.conv1.parameters():
-            p.requires_grad = False
-        for p in self.resnet50.bn1.parameters():
-            p.requires_grad = False
-
-    def trainable_parameters(self):
-        return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
-
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -88,6 +47,7 @@ class Net(nn.Module):
         x = self.stage3(x)
         x = self.stage4(x)
         feat = torchutils.gap2d(x, keepdims=True)
+        print(feat.shape)
         x = self.classifier(feat)
         x = x.view(-1, 20)
         return x, feat
@@ -141,54 +101,7 @@ class CAM(nn.Module):
     def trainable_parameters(self):
         return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
 
-
-class IntervenedCAM(nn.Module):
-    def __init__(self, cam_dir, cam_square):
-        super(IntervenedCAM, self).__init__()
-        self.resnet50 = resnet50.resnet50(
-            pretrained=True, strides=(2, 2, 2, 1))
-
-        self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1, self.resnet50.relu, self.resnet50.maxpool,
-                                    self.resnet50.layer1)
-        self.stage2 = nn.Sequential(self.resnet50.layer2)
-        self.stage3 = nn.Sequential(self.resnet50.layer3)
-        self.stage4 = nn.Sequential(self.resnet50.layer4)
-
-        self.classifier = nn.Conv2d(2048, 20, 1, bias=False)
-
-        self.backbone = nn.ModuleList(
-            [self.stage1, self.stage2, self.stage3, self.stage4])
-        self.newly_added = nn.ModuleList([self.classifier])
-
-        cam_is_empty = len(os.listdir(cam_dir)) == 0
-        if cam_is_empty:
-            self.scams = torch.ones(20, cam_square, cam_square)
-        else:
-            self.scams = pyutils.sum_cams(cam_dir)
-
-    def forward(self, x):
-        x = self.stage1(x)
-        x = self.stage2(x)
-        x = self.stage3(x)
-        x = self.stage4(x)
-        x = F.conv2d(x, self.classifier.weight)
-
-        x = x * F.interpolate(self.scams.unsqueeze(0), size=x.shape[2:],
-                              mode='bilinear', align_corners=False).cuda(non_blocking=True)
-
-        x = F.relu(x)
-        x = x[0] + x[1].flip(-1)
-
-        return x
-
-    def train(self, mode=True):
-        for p in self.resnet50.conv1.parameters():
-            p.requires_grad = False
-        for p in self.resnet50.bn1.parameters():
-            p.requires_grad = False
-
-    def trainable_parameters(self):
-        return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
+# === Class Re-Activation Map
 
 
 class ReNet(nn.Module):
