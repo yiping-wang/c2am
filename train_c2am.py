382620c8193d601c4d0d08d821dc9d2a2079b179
Yiping Wang <yiping.wang@gmx.com>
2022-03-21 16:14:54 -0400

    .

--- train_c2am.py
+++ train_c2am.py
@@ -4,7 +4,6 @@ import argparse
 import torch
 import os
 import numpy as np
-from PIL import Image
 from torch.utils.data import DataLoader
 from misc import pyutils, torchutils
 from net.resnet50_cam import Net, MLP
@@ -116,8 +115,8 @@ def train(config, config_path):
     # os.system('python3 make_square_cam.py --config {}'.format(config_path))
     # global_cams = pyutils.sum_cams(cam_out_dir).cuda(non_blocking=True)
     # np.save(scam_path, global_cams.cpu().numpy())
-    global_cams = pyutils.sum_cams(os.path.join(
-        scam_out_dir, 'global_cam.npy')).cuda(non_blocking=True)
+    global_cams = torch.from_numpy(np.load(os.path.join(
+        scam_out_dir, 'global_cam.npy'))).cuda(non_blocking=True)
     # ===
     for ep in range(cam_num_epoches):
         print('Epoch %d/%d' % (ep+1, cam_num_epoches))
