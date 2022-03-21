7c87c2786d2b3225925144160b0ad4a324740477
Yiping Wang <yiping.wang@gmx.com>
2022-03-21 19:39:18 -0400

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -7,7 +7,7 @@ import os
 
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim=2048, output_dim=128):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_dim, input_dim)
         self.bn1 = nn.BatchNorm1d(input_dim)
