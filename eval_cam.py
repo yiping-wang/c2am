8d4e83bf38fd99d45324d6fc35b91c7da919f4c1
Yiping Wang <yiping.wang@gmx.com>
2022-03-21 15:25:02 -0400

    .

--- eval_cam.py
+++ eval_cam.py
@@ -1,17 +1,24 @@
 import numpy as np
 import os
+import argparse
+from misc import pyutils
 from chainercv.datasets import VOCSemanticSegmentationDataset
 from chainercv.evaluations import calc_semantic_segmentation_confusion
 
-def run(args):
-    dataset = VOCSemanticSegmentationDataset(split=args.chainer_eval_set, data_dir=args.voc12_root)
+def run(config):
+    chainer_eval_set = config['chainer_eval_set']
+    voc12_root = config['voc12_root']
+    cam_out_dir = config['cam_out_dir']
+    cam_eval_thres = config['cam_eval_thres']
+
+    dataset = VOCSemanticSegmentationDataset(split=chainer_eval_set, data_dir=voc12_root)
     labels = [dataset.get_example_by_keys(i, (1,))[0] for i in range(len(dataset))]
 
     preds = []
     for id in dataset.ids:
-        cam_dict = np.load(os.path.join(args.cam_out_dir, id + '.npy'), allow_pickle=True).item()
+        cam_dict = np.load(os.path.join(cam_out_dir, id + '.npy'), allow_pickle=True).item()
         cams = cam_dict['high_res']
-        cams = np.pad(cams, ((1, 0), (0, 0), (0, 0)), mode='constant', constant_values=args.cam_eval_thres)
+        cams = np.pad(cams, ((1, 0), (0, 0), (0, 0)), mode='constant', constant_values=cam_eval_thres)
         keys = np.pad(cam_dict['keys'] + 1, (1, 0), mode='constant')
         cls_labels = np.argmax(cams, axis=0)
         cls_labels = keys[cls_labels]
@@ -26,3 +33,12 @@ def run(args):
     iou = gtjresj / denominator
 
     print({'iou': iou, 'miou': np.nanmean(iou)})
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Front Door Semantic Segmentation')
+    parser.add_argument('--config', type=str,
+                        help='YAML config file path', requires=True)
+    args = parser.parse_args()
+    config = pyutils.parse_config(args.config)
+    run(config)
--- run_c2am.sh
+++ run_c2am.sh
@@ -41,8 +41,8 @@ rm /data/home/yipingwang/data/SemSegC2AM/*
 # python3 train_c2am.py             --config    ./cfg/c2am_exp_01.yml
 python3 make_cam.py               --config    ./cfg/c2am_exp_01.yml
 python3 eval_cam.py               --config    ./cfg/c2am_exp_01.yml
-python3 cam_to_ir_label.py        --config    ./cfg/irn_c2am_exp_01.yml
-python3 train_irn.py              --config    ./cfg/irn_c2am_exp_01.yml
-python3 make_sem_seg_labels.py    --config    ./cfg/irn_c2am_exp_01.yml
-python3 eval_sem_seg.py           --config    ./cfg/irn_c2am_exp_01.yml
+# python3 cam_to_ir_label.py        --config    ./cfg/irn_c2am_exp_01.yml
+# python3 train_irn.py              --config    ./cfg/irn_c2am_exp_01.yml
+# python3 make_sem_seg_labels.py    --config    ./cfg/irn_c2am_exp_01.yml
+# python3 eval_sem_seg.py           --config    ./cfg/irn_c2am_exp_01.yml
 echo "======================================="
