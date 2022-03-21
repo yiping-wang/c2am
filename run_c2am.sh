7dee68d0c090dc74c563ca7044326eff9b6959f9
Yiping Wang <yiping.wang@gmx.com>
2022-03-21 15:33:20 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -31,15 +31,15 @@
 # echo "======================================="
 
 echo "======= Exp C2AM Exp 01 ========="
-mkdir -p /data/home/yipingwang/data/C2AM/
-mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
-mkdir -p /data/home/yipingwang/data/SemSegC2AM/
-mkdir -p /data/home/yipingwang/data/GlobalCAM/
-rm /data/home/yipingwang/data/C2AM/*
-rm /data/home/yipingwang/data/IRLabelC2AM/*
-rm /data/home/yipingwang/data/SemSegC2AM/*
+# mkdir -p /data/home/yipingwang/data/C2AM/
+# mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
+# mkdir -p /data/home/yipingwang/data/SemSegC2AM/
+# mkdir -p /data/home/yipingwang/data/GlobalCAM/
+# rm /data/home/yipingwang/data/C2AM/*
+# rm /data/home/yipingwang/data/IRLabelC2AM/*
+# rm /data/home/yipingwang/data/SemSegC2AM/*
 # python3 train_c2am.py             --config    ./cfg/c2am_exp_01.yml
-python3 make_cam.py               --config    ./cfg/c2am_exp_01.yml
+# python3 make_cam.py               --config    ./cfg/c2am_exp_01.yml
 python3 eval_cam.py               --config    ./cfg/c2am_exp_01.yml
 # python3 cam_to_ir_label.py        --config    ./cfg/irn_c2am_exp_01.yml
 # python3 train_irn.py              --config    ./cfg/irn_c2am_exp_01.yml
