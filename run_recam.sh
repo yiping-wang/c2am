61401119d2d3e5af57e9ee4c7e61692e3883a916
Yiping Wang <yiping.wang@gmx.com>
2022-03-22 17:13:18 -0400

    .

--- run_recam.sh
+++ run_recam.sh
@@ -1,18 +1,12 @@
-echo "======= ReCAM Exp 03 ========="
-mkdir -p /data/home/yipingwang/data/CAMReCAM/
-mkdir -p /data/home/yipingwang/data/IRLabelReCAM/
-mkdir -p /data/home/yipingwang/data/SemSegReCAM/
-rm /data/home/yipingwang/data/CAMReCAM/*
-rm /data/home/yipingwang/data/IRLabelReCAM/*
-rm /data/home/yipingwang/data/SemSegReCAM/*
-python3 train_recam.py           --config ./cfg/recam/recam_exp_03.yml
-python3 make_recam.py            --config ./cfg/recam/recam_exp_03.yml
-python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.21
-python3 cam_to_ir_label.py       --config ./cfg/recam/recam_exp_03.yml
-python3 train_irn.py             --config ./cfg/recam/recam_exp_03.yml
-python3 make_sem_seg_labels.py   --config ./cfg/recam/recam_exp_03.yml
-python3 eval_sem_seg.py          --config ./cfg/recam/recam_exp_03.yml
-echo "========================"
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.15
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.16
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.17
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.18
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.19
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.20
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.22
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.23
+
 
 echo "======= ReCAM Exp 01 ========="
 mkdir -p /data/home/yipingwang/data/CAMReCAM/
@@ -44,4 +38,20 @@ python3 cam_to_ir_label.py       --config ./cfg/recam/recam_exp_02.yml
 python3 train_irn.py             --config ./cfg/recam/recam_exp_02.yml
 python3 make_sem_seg_labels.py   --config ./cfg/recam/recam_exp_02.yml
 python3 eval_sem_seg.py          --config ./cfg/recam/recam_exp_02.yml
+echo "========================"
+
+echo "======= ReCAM Exp 03 ========="
+mkdir -p /data/home/yipingwang/data/CAMReCAM/
+mkdir -p /data/home/yipingwang/data/IRLabelReCAM/
+mkdir -p /data/home/yipingwang/data/SemSegReCAM/
+rm /data/home/yipingwang/data/CAMReCAM/*
+rm /data/home/yipingwang/data/IRLabelReCAM/*
+rm /data/home/yipingwang/data/SemSegReCAM/*
+python3 train_recam.py           --config ./cfg/recam/recam_exp_03.yml
+python3 make_recam.py            --config ./cfg/recam/recam_exp_03.yml
+python3 eval_cam.py              --config ./cfg/recam/recam_exp_03.yml --cam_eval_thres 0.21
+python3 cam_to_ir_label.py       --config ./cfg/recam/recam_exp_03.yml
+python3 train_irn.py             --config ./cfg/recam/recam_exp_03.yml
+python3 make_sem_seg_labels.py   --config ./cfg/recam/recam_exp_03.yml
+python3 eval_sem_seg.py          --config ./cfg/recam/recam_exp_03.yml
 echo "========================"
