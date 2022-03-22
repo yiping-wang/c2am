1dde9af2e762d4299a72758da6ec18ef0b187afb
Yiping Wang <yiping.wang@gmx.com>
2022-03-22 14:10:59 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -1,3 +1,19 @@
+echo "======= Exp C2AM Exp 01 Val ========="
+mkdir -p /data/home/yipingwang/data/C2AM/
+mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
+mkdir -p /data/home/yipingwang/data/SemSegC2AM/
+mkdir -p /data/home/yipingwang/data/GlobalCAM/
+# rm /data/home/yipingwang/data/C2AM/*
+# rm /data/home/yipingwang/data/IRLabelC2AM/*
+# rm /data/home/yipingwang/data/SemSegC2AM/*
+# python3 make_cam.py               --config    ./cfg/c2am_exp_01.yml
+python3 eval_cam.py               --config    ./cfg/c2am_exp_01.yml --cam_eval_thres 0.15
+# python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_01.yml
+# python3 train_irn.py              --config    ./cfg/c2am_exp_01.yml
+# python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_01.yml
+# python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_01.yml
+echo "======================================="
+
 echo "======= Exp C2AM Exp 02 Val ========="
 mkdir -p /data/home/yipingwang/data/C2AM/
 mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
@@ -7,25 +23,9 @@ rm /data/home/yipingwang/data/C2AM/*
 rm /data/home/yipingwang/data/IRLabelC2AM/*
 rm /data/home/yipingwang/data/SemSegC2AM/*
 python3 make_cam.py               --config    ./cfg/c2am_exp_02.yml
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.15
+python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.15
 # python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_02.yml
 # python3 train_irn.py              --config    ./cfg/c2am_exp_02.yml
 # python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_02.yml
 # python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_02.yml
-echo "======================================="
-
-echo "======= Exp C2AM Exp 01 Val ========="
-mkdir -p /data/home/yipingwang/data/C2AM/
-mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
-mkdir -p /data/home/yipingwang/data/SemSegC2AM/
-mkdir -p /data/home/yipingwang/data/GlobalCAM/
-rm /data/home/yipingwang/data/C2AM/*
-rm /data/home/yipingwang/data/IRLabelC2AM/*
-rm /data/home/yipingwang/data/SemSegC2AM/*
-python3 make_cam.py               --config    ./cfg/c2am_exp_01.yml
-python3 eval_cam.py               --config    ./cfg/c2am_exp_01.yml --cam_eval_thres 0.15
-# python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_01.yml
-# python3 train_irn.py              --config    ./cfg/c2am_exp_01.yml
-# python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_01.yml
-# python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_01.yml
 echo "======================================="
