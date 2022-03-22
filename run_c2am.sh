ef7289a77a1c61dbc4a3fd1d235b1b59f9cb3df9
Yiping Wang <yiping.wang@gmx.com>
2022-03-22 14:37:06 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -3,10 +3,10 @@ mkdir -p /data/home/yipingwang/data/C2AM/
 mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
 mkdir -p /data/home/yipingwang/data/SemSegC2AM/
 mkdir -p /data/home/yipingwang/data/GlobalCAM/
-# rm /data/home/yipingwang/data/C2AM/*
-# rm /data/home/yipingwang/data/IRLabelC2AM/*
-# rm /data/home/yipingwang/data/SemSegC2AM/*
-# python3 make_cam.py               --config    ./cfg/c2am_exp_01.yml
+rm /data/home/yipingwang/data/C2AM/*
+rm /data/home/yipingwang/data/IRLabelC2AM/*
+rm /data/home/yipingwang/data/SemSegC2AM/*
+python3 make_cam.py               --config    ./cfg/c2am_exp_01.yml
 python3 eval_cam.py               --config    ./cfg/c2am_exp_01.yml --cam_eval_thres 0.15
 # python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_01.yml
 # python3 train_irn.py              --config    ./cfg/c2am_exp_01.yml
