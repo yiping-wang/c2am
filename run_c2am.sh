f734338e5e892fbdab001a9edb2d5af9c518b3ce
Yiping Wang <yiping.wang@gmx.com>
2022-03-22 09:28:06 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -1,4 +1,4 @@
-echo "======= Exp C2AM Exp 02 ========="
+# echo "======= Exp C2AM Exp 02 ========="
 # mkdir -p /data/home/yipingwang/data/C2AM/
 # mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
 # mkdir -p /data/home/yipingwang/data/SemSegC2AM/
@@ -17,11 +17,11 @@ echo "======= Exp C2AM Exp 02 ========="
 # python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.21
 # python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.22
 # python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.23
-python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_02.yml
-python3 train_irn.py              --config    ./cfg/c2am_exp_02.yml
-python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_02.yml
-python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_02.yml
-echo "======================================="
+# python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_02.yml
+# python3 train_irn.py              --config    ./cfg/c2am_exp_02.yml
+# python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_02.yml
+# python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_02.yml
+# echo "======================================="
 
 echo "======= Exp C2AM Exp 03 ========="
 mkdir -p /data/home/yipingwang/data/C2AM/
@@ -31,17 +31,17 @@ mkdir -p /data/home/yipingwang/data/GlobalCAM/
 rm /data/home/yipingwang/data/C2AM/*
 rm /data/home/yipingwang/data/IRLabelC2AM/*
 rm /data/home/yipingwang/data/SemSegC2AM/*
-python3 train_c2am.py             --config    ./cfg/c2am_exp_03.yml
+# python3 train_c2am.py             --config    ./cfg/c2am_exp_03.yml
 python3 make_cam.py               --config    ./cfg/c2am_exp_03.yml
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.15
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.16
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.17
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.18
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.19
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.20
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.21
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.22
-python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.23
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.15
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.16
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.17
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.18
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.19
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.20
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.21
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.22
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_03.yml --cam_eval_thres 0.23
 python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_03.yml
 python3 train_irn.py              --config    ./cfg/c2am_exp_03.yml
 python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_03.yml
@@ -56,17 +56,17 @@ mkdir -p /data/home/yipingwang/data/GlobalCAM/
 rm /data/home/yipingwang/data/C2AM/*
 rm /data/home/yipingwang/data/IRLabelC2AM/*
 rm /data/home/yipingwang/data/SemSegC2AM/*
-python3 train_c2am.py             --config    ./cfg/c2am_exp_04.yml
+# python3 train_c2am.py             --config    ./cfg/c2am_exp_04.yml
 python3 make_cam.py               --config    ./cfg/c2am_exp_04.yml
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.15
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.16
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.17
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.18
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.19
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.20
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.21
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.22
-python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.23
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.15
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.16
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.17
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.18
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.19
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.20
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.21
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.22
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_04.yml --cam_eval_thres 0.23
 python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_04.yml
 python3 train_irn.py              --config    ./cfg/c2am_exp_04.yml
 python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_04.yml
@@ -81,17 +81,17 @@ mkdir -p /data/home/yipingwang/data/GlobalCAM/
 rm /data/home/yipingwang/data/C2AM/*
 rm /data/home/yipingwang/data/IRLabelC2AM/*
 rm /data/home/yipingwang/data/SemSegC2AM/*
-python3 train_c2am.py             --config    ./cfg/c2am_exp_05.yml
+# python3 train_c2am.py             --config    ./cfg/c2am_exp_05.yml
 python3 make_cam.py               --config    ./cfg/c2am_exp_05.yml
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.15
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.16
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.17
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.18
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.19
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.20
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.21
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.22
-python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.23
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.15
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.16
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.17
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.18
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.19
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.20
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.21
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.22
+# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.23
 python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_05.yml
 python3 train_irn.py              --config    ./cfg/c2am_exp_05.yml
 python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_05.yml
