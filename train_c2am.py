6b9bdd184bc8d26ba9658f829e0af6aea782c384
Yiping Wang <yiping.wang@gmx.com>
2022-03-22 10:39:18 -0400

    .

--- train_c2am.py
+++ train_c2am.py
@@ -111,11 +111,11 @@ def train(config, config_path):
     timer = pyutils.Timer()
     # P(y|x, z)
     # generate Global CAMs
-    # os.system('python3 make_square_cam.py --config {}'.format(config_path))
-    # global_cams = pyutils.sum_cams(cam_out_dir).cuda(non_blocking=True)
-    # np.save(scam_path, global_cams.cpu().numpy())
-    global_cams = torch.from_numpy(np.load(os.path.join(
-        scam_out_dir, 'global_cam_01.npy'))).cuda(non_blocking=True)
+    os.system('python3 make_square_cam.py --config {}'.format(config_path))
+    global_cams = pyutils.sum_cams(cam_out_dir).cuda(non_blocking=True)
+    np.save(scam_path, global_cams.cpu().numpy())
+    # global_cams = torch.from_numpy(np.load(os.path.join(
+    #     scam_out_dir, 'global_cam_01.npy'))).cuda(non_blocking=True)
     # ===
     for ep in range(cam_num_epoches):
         print('Epoch %d/%d' % (ep+1, cam_num_epoches))
