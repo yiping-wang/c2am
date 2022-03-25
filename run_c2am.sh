a8fd3fff03e915593bcc0d1ed18dc0315f4b5da0
Yiping Wang <yiping.wang@gmx.com>
2022-03-25 00:16:38 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -8,7 +8,13 @@ rm /data/home/yipingwang/data/IRLabelC2AM/*
 rm /data/home/yipingwang/data/SemSegC2AM/*
 python3 train_c2am2.py            --config    ./cfg/c2am_exp_02.yml
 python3 make_cam.py               --config    ./cfg/c2am_exp_02.yml
+python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.13
 python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.15
+python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.17
+python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.19
+python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.21
+python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.23
+python3 eval_cam.py               --config    ./cfg/c2am_exp_02.yml --cam_eval_thres 0.25
 python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_02.yml
 python3 train_irn.py              --config    ./cfg/c2am_exp_02.yml
 python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_02.yml
