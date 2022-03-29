e4d862bf9cd2bbc6bf1e8031d86cd455ae987436
Yiping Wang <yiping.wang@gmx.com>
2022-03-29 11:08:28 -0400

    .

--- train_c2am.py
+++ train_c2am.py
@@ -1,4 +1,4 @@
-import torch.nn.functional as F
+
 import numpy as np
 import voc12.dataloader
 import argparse
@@ -6,8 +6,7 @@ import torch
 import os
 from torch.utils.data import DataLoader
 from misc import pyutils, torchutils
-from net.resnet50_cam import Net, MLP
-from voc12.dataloader import get_img_path
+from net.resnet50_cam import Net
 
 
 def validate(cls_model, data_loader):
@@ -37,21 +36,18 @@ def train(config):
     cam_batch_size = config['cam_batch_size']
     cam_num_epoches = config['cam_num_epoches']
     cam_learning_rate = config['cam_learning_rate']
-    sty_learning_rate = config['sty_learning_rate']
     cam_weight_decay = config['cam_weight_decay']
     cam_crop_size = config['cam_crop_size']
     model_root = config['model_root']
     cam_weights_name = config['cam_weights_name']
     agg_smooth_r = config['agg_smooth_r']
     num_workers = config['num_workers']
-    alpha = config['alpha']
     scam_out_dir = config['scam_out_dir']
     laste_cam_weights_name = config['laste_cam_weights_name']
     cam_weight_path = os.path.join(model_root, cam_weights_name)
     laste_cam_weight_path = os.path.join(model_root, laste_cam_weights_name)
 
     pyutils.seed_all(seed)
-    data_aug_fn = torchutils.get_simclr_pipeline_transform(size=cam_crop_size)
 
     # CAM generation dataset
     train_dataset = voc12.dataloader.VOC12ClassificationDataset(train_list, voc12_root=voc12_root,
@@ -76,7 +72,6 @@ def train(config):
                                  drop_last=True)
 
     cls_model = Net()
-    mlp = MLP()
 
     # load the pre-trained weights
     cls_model.load_state_dict(torch.load(cam_weight_path), strict=True)
@@ -87,20 +82,14 @@ def train(config):
             'weight_decay': cam_weight_decay},
         {'params': param_groups[1], 'lr': 10 * cam_learning_rate,
             'weight_decay': cam_weight_decay},
-        {'params': mlp.parameters(), 'lr': sty_learning_rate,
-            'weight_decay': cam_weight_decay},
     ], lr=cam_learning_rate, weight_decay=cam_weight_decay, max_step=max_step)
 
     cls_model.train()
-    mlp.train()
-
     # Parallel
     cls_model = torch.nn.DataParallel(cls_model).cuda()
-    mlp = torch.nn.DataParallel(mlp).cuda() if alpha > 0 else MLP()
 
-    avg_meter = pyutils.AverageMeter('loss', 'bce', 'kl')
+    avg_meter = pyutils.AverageMeter('loss')
     timer = pyutils.Timer()
-    kl_loss = torch.tensor(0.)
     # P(y|x, z)
     # generate Global CAMs
     # os.system('python3 make_square_cam.py --config {}'.format(config_path))
@@ -117,7 +106,6 @@ def train(config):
     for ep in range(cam_num_epoches):
         print('Epoch %d/%d' % (ep+1, cam_num_epoches))
         for step, pack in enumerate(train_data_loader):
-            names = pack['name']
             imgs = pack['img'].cuda(non_blocking=True)
             labels = pack['label'].cuda(non_blocking=True)
             # Front Door Adjustment
@@ -128,34 +116,9 @@ def train(config):
             # Aggregate for classification
             # agg(P(z|x) * sum(P(y|x, z) * P(x)))
             x = torchutils.mean_agg(x, r=agg_smooth_r)
-            # Entropy loss for Content Adjustment
-            bce_loss = torch.nn.BCEWithLogitsLoss()(x, labels)
-            if alpha > 0:
-                # Style Intervention from Eq. 3 at 2010.07922
-                augs = torch.cat([torchutils.get_style_variants(
-                    names, data_aug_fn, voc12_root, get_img_path) for _ in range(4)], dim=0)
-                _, feats = cls_model(augs)
-                projs = mlp(feats)
-                norms = F.normalize(projs, dim=1)
-                proj_l, proj_k, proj_q, proj_t = torch.split(
-                    norms, split_size_or_sections=cam_batch_size, dim=0)
-                # cosine similarity of each feature in l to all features in k
-                # each row of score_lk is an image in k and its similarity to all features in l
-                # each col of score_lk is an image in l and its similarity to all features in k
-                # applies the same to another set of augmentations qt
-                # then minimizes the KL-divergence of kl and qt
-                score_lk = torch.matmul(proj_l, proj_k.T)
-                score_qt = torch.matmul(proj_q, proj_t.T)
-                logprob_lk = F.log_softmax(score_lk, dim=1)
-                prob_qt = F.softmax(score_qt, dim=1)
-                # KL-divergence loss for Style Intervention
-                kl_loss = alpha * \
-                    torch.nn.KLDivLoss(reduction='batchmean')(
-                        logprob_lk, prob_qt)
-            # Loss
-            loss = bce_loss + kl_loss if alpha > 0 else bce_loss
-            avg_meter.add(
-                {'loss': loss.item(), 'bce': bce_loss.item(), 'kl': kl_loss.item()})
+            # Entropy loss for Multiple-Instance Learning
+            loss = torch.nn.BCEWithLogitsLoss()(x, labels)
+            avg_meter.add({'loss': loss.item()})
             # Optimization
             optimizer.zero_grad()
             loss.backward()
@@ -165,10 +128,8 @@ def train(config):
                 timer.update_progress(optimizer.global_step / max_step)
                 print('step:%5d/%5d' % (optimizer.global_step - 1, max_step),
                       'Loss:%.4f' % (avg_meter.pop('loss')),
-                      'BCE:%.4f' % (avg_meter.pop('bce')),
-                      'KL:%.4f' % (avg_meter.pop('kl')),
-                      'imps:%.1f' % (
-                          (step + 1) * cam_batch_size / timer.get_stage_elapsed()),
+                      'imps:%.1f' % ((step + 1) * cam_batch_size /
+                                     timer.get_stage_elapsed()),
                       'lr: %.4f' % (optimizer.param_groups[0]['lr']),
                       'etc:%s' % (timer.str_estimated_complete()), flush=True)
                 validate(cls_model, val_data_loader)
