ae0cef43d08a397fdcc83070eae60ab7301e450c
Yiping Wang <yiping.wang@gmx.com>
2022-03-31 16:26:49 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -1,19 +1,6 @@
 echo "======= Exp C2AM Exp 05 Train ========="
-mkdir -p /data/home/yipingwang/data/C2AM/
-mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
-mkdir -p /data/home/yipingwang/data/SemSegC2AM/
-mkdir -p /data/home/yipingwang/data/GlobalCAM/
-# rm /data/home/yipingwang/data/C2AM/*
 rm /data/home/yipingwang/data/IRLabelC2AM/*
 rm /data/home/yipingwang/data/SemSegC2AM/*
-# python3 train_c2am.py             --config    ./cfg/c2am_exp_05.yml
-# python3 make_cam.py               --config    ./cfg/c2am_exp_05.yml
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.10
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.12
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.14
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.16
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.18
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_05.yml --cam_eval_thres 0.20
 python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_05.yml
 python3 train_irn.py              --config    ./cfg/c2am_exp_05.yml
 python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_05.yml
@@ -21,25 +8,39 @@ python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_05.yml
 echo "======================================="
 
 echo "======= Exp C2AM Exp 06 Train ========="
-mkdir -p /data/home/yipingwang/data/C2AM/
-mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
-mkdir -p /data/home/yipingwang/data/SemSegC2AM/
-mkdir -p /data/home/yipingwang/data/GlobalCAM/
-# rm /data/home/yipingwang/data/C2AM/*
 rm /data/home/yipingwang/data/IRLabelC2AM/*
 rm /data/home/yipingwang/data/SemSegC2AM/*
-# python3 train_c2am.py             --config    ./cfg/c2am_exp_06.yml
-# python3 make_cam.py               --config    ./cfg/c2am_exp_06.yml
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_06.yml --cam_eval_thres 0.10
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_06.yml --cam_eval_thres 0.12
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_06.yml --cam_eval_thres 0.14
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_06.yml --cam_eval_thres 0.16
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_06.yml --cam_eval_thres 0.18
-# python3 eval_cam.py               --config    ./cfg/c2am_exp_06.yml --cam_eval_thres 0.20
 python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_06.yml
 python3 train_irn.py              --config    ./cfg/c2am_exp_06.yml
 python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_06.yml
 python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_06.yml
+echo "======================================="
+
+echo "======= Exp C2AM Exp 07 Train ========="
+rm /data/home/yipingwang/data/IRLabelC2AM/*
+rm /data/home/yipingwang/data/SemSegC2AM/*
+python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_07.yml
+python3 train_irn.py              --config    ./cfg/c2am_exp_07.yml
+python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_07.yml
+python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_07.yml
+echo "======================================="
+
+echo "======= Exp C2AM Exp 08 Train ========="
+rm /data/home/yipingwang/data/IRLabelC2AM/*
+rm /data/home/yipingwang/data/SemSegC2AM/*
+python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_08.yml
+python3 train_irn.py              --config    ./cfg/c2am_exp_08.yml
+python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_08.yml
+python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_08.yml
+echo "======================================="
+
+echo "======= Exp C2AM Exp 09 Train ========="
+rm /data/home/yipingwang/data/IRLabelC2AM/*
+rm /data/home/yipingwang/data/SemSegC2AM/*
+python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp_09.yml
+python3 train_irn.py              --config    ./cfg/c2am_exp_09.yml
+python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp_09.yml
+python3 eval_sem_seg.py           --config    ./cfg/c2am_exp_09.yml
 echo "======================================="
 
 # echo "======= Exp C2AM Exp 04 Train ========="
