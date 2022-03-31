ee027211b3a3340f1e161923de565f800c9b47ef
Yiping Wang <yiping.wang@gmx.com>
2022-03-31 12:35:37 -0400

    .

--- train_c2am.py
+++ train_c2am.py
@@ -47,93 +47,93 @@ def train(config, config_path):
     cam_weight_path = os.path.join(model_root, cam_weights_name)
     laste_cam_weight_path = os.path.join(model_root, laste_cam_weights_name)
 
-    # pyutils.seed_all(seed)
+    pyutils.seed_all(seed)
 
-    # # CAM generation dataset
-    # train_dataset = voc12.dataloader.VOC12ClassificationDataset(train_list, voc12_root=voc12_root,
-    #                                                             resize_long=(320, 640), hor_flip=True,
-    #                                                             crop_size=cam_crop_size, crop_method="random")
-    # train_data_loader = DataLoader(train_dataset,
-    #                                batch_size=cam_batch_size,
-    #                                shuffle=True,
-    #                                num_workers=num_workers,
-    #                                pin_memory=True,
-    #                                drop_last=True)
+    # CAM generation dataset
+    train_dataset = voc12.dataloader.VOC12ClassificationDataset(train_list, voc12_root=voc12_root,
+                                                                resize_long=(320, 640), hor_flip=True,
+                                                                crop_size=cam_crop_size, crop_method="random")
+    train_data_loader = DataLoader(train_dataset,
+                                   batch_size=cam_batch_size,
+                                   shuffle=True,
+                                   num_workers=num_workers,
+                                   pin_memory=True,
+                                   drop_last=True)
 
-    # max_step = (len(train_dataset) // cam_batch_size) * cam_num_epoches
+    max_step = (len(train_dataset) // cam_batch_size) * cam_num_epoches
 
-    # val_dataset = voc12.dataloader.VOC12ClassificationDataset(val_list, voc12_root=voc12_root,
-    #                                                           crop_size=cam_crop_size)
-    # val_data_loader = DataLoader(val_dataset,
-    #                              batch_size=cam_batch_size,
-    #                              shuffle=False,
-    #                              num_workers=num_workers,
-    #                              pin_memory=True,
-    #                              drop_last=True)
+    val_dataset = voc12.dataloader.VOC12ClassificationDataset(val_list, voc12_root=voc12_root,
+                                                              crop_size=cam_crop_size)
+    val_data_loader = DataLoader(val_dataset,
+                                 batch_size=cam_batch_size,
+                                 shuffle=False,
+                                 num_workers=num_workers,
+                                 pin_memory=True,
+                                 drop_last=True)
 
-    # cls_model = Net()
+    cls_model = Net()
 
-    # # load the pre-trained weights
-    # cls_model.load_state_dict(torch.load(cam_weight_path), strict=True)
+    # load the pre-trained weights
+    cls_model.load_state_dict(torch.load(cam_weight_path), strict=True)
 
-    # param_groups = cls_model.trainable_parameters()
-    # optimizer = torchutils.PolyOptimizer([
-    #     {'params': param_groups[0], 'lr': cam_learning_rate,
-    #         'weight_decay': cam_weight_decay},
-    #     {'params': param_groups[1], 'lr': 10 * cam_learning_rate,
-    #         'weight_decay': cam_weight_decay},
-    # ], lr=cam_learning_rate, weight_decay=cam_weight_decay, max_step=max_step)
+    param_groups = cls_model.trainable_parameters()
+    optimizer = torchutils.PolyOptimizer([
+        {'params': param_groups[0], 'lr': cam_learning_rate,
+            'weight_decay': cam_weight_decay},
+        {'params': param_groups[1], 'lr': 10 * cam_learning_rate,
+            'weight_decay': cam_weight_decay},
+    ], lr=cam_learning_rate, weight_decay=cam_weight_decay, max_step=max_step)
 
-    # cls_model.train()
-    # # Parallel
-    # cls_model = torch.nn.DataParallel(cls_model).cuda()
+    cls_model.train()
+    # Parallel
+    cls_model = torch.nn.DataParallel(cls_model).cuda()
 
-    # avg_meter = pyutils.AverageMeter('loss')
-    # timer = pyutils.Timer()
+    avg_meter = pyutils.AverageMeter('loss')
+    timer = pyutils.Timer()
     # P(y|x, z)
     # generate Global CAMs
-    os.system('python3 make_square_cam.py --config {}'.format(config_path))
+    # os.system('python3 make_square_cam.py --config {}'.format(config_path))
     # global_cams = pyutils.sum_cams(config['cam_out_dir']).cuda(non_blocking=True)
-    global_cams = pyutils.sum_cams_by_class(config['cam_out_dir']).cuda(non_blocking=True)
-    np.save(os.path.join(scam_out_dir, config['scam_name']), global_cams.cpu().numpy())
-    # global_cams = torch.from_numpy(np.load(os.path.join(
-    #     scam_out_dir, 'global_cam_by_class.npy'))).cuda(non_blocking=True)
+    # global_cams = pyutils.sum_cams_by_class(config['cam_out_dir']).cuda(non_blocking=True)
+    # np.save(os.path.join(scam_out_dir, config['scam_name']), global_cams.cpu().numpy())
+    global_cams = torch.from_numpy(np.load(os.path.join(
+        scam_out_dir, 'c2am_exp_02_global_cam.npy'))).cuda(non_blocking=True)
     # # ===
-    # for ep in range(cam_num_epoches):
-    #     print('Epoch %d/%d' % (ep+1, cam_num_epoches))
-    #     for step, pack in enumerate(train_data_loader):
-    #         imgs = pack['img'].cuda(non_blocking=True)
-    #         labels = pack['label'].cuda(non_blocking=True)
-    #         # Front Door Adjustment
-    #         # P(z|x)
-    #         x, _ = cls_model(imgs)
-    #         # P(y|do(x))
-    #         x = x.unsqueeze(2).unsqueeze(2) * global_cams
-    #         # Aggregate for classification
-    #         # agg(P(z|x) * sum(P(y|x, z) * P(x)))
-    #         x = torchutils.mean_agg(x, r=agg_smooth_r)
-    #         # Entropy loss for Multiple-Instance Learning
-    #         loss = torch.nn.BCEWithLogitsLoss()(x, labels)
-    #         avg_meter.add({'loss': loss.item()})
-    #         # Optimization
-    #         optimizer.zero_grad()
-    #         loss.backward()
-    #         optimizer.step()
-    #         # Progress
-    #         if (optimizer.global_step - 1) % 100 == 0:
-    #             timer.update_progress(optimizer.global_step / max_step)
-    #             print('step:%5d/%5d' % (optimizer.global_step - 1, max_step),
-    #                   'Loss:%.4f' % (avg_meter.pop('loss')),
-    #                   'imps:%.1f' % ((step + 1) * cam_batch_size /
-    #                                  timer.get_stage_elapsed()),
-    #                   'lr: %.4f' % (optimizer.param_groups[0]['lr']),
-    #                   'etc:%s' % (timer.str_estimated_complete()), flush=True)
-    #             validate(cls_model, val_data_loader)
-    #         else:
-    #             timer.reset_stage()
-    #     # Empty cache
-    #     torch.save(cls_model.module.state_dict(), laste_cam_weight_path)
-    #     torch.cuda.empty_cache()
+    for ep in range(cam_num_epoches):
+        print('Epoch %d/%d' % (ep+1, cam_num_epoches))
+        for step, pack in enumerate(train_data_loader):
+            imgs = pack['img'].cuda(non_blocking=True)
+            labels = pack['label'].cuda(non_blocking=True)
+            # Front Door Adjustment
+            # P(z|x)
+            x, _ = cls_model(imgs)
+            # P(y|do(x))
+            x = x.unsqueeze(2).unsqueeze(2) * global_cams
+            # Aggregate for classification
+            # agg(P(z|x) * sum(P(y|x, z) * P(x)))
+            x = torchutils.mean_agg(x, r=agg_smooth_r)
+            # Entropy loss for Multiple-Instance Learning
+            loss = torch.nn.BCEWithLogitsLoss()(x, labels)
+            avg_meter.add({'loss': loss.item()})
+            # Optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            # Progress
+            if (optimizer.global_step - 1) % 100 == 0:
+                timer.update_progress(optimizer.global_step / max_step)
+                print('step:%5d/%5d' % (optimizer.global_step - 1, max_step),
+                      'Loss:%.4f' % (avg_meter.pop('loss')),
+                      'imps:%.1f' % ((step + 1) * cam_batch_size /
+                                     timer.get_stage_elapsed()),
+                      'lr: %.4f' % (optimizer.param_groups[0]['lr']),
+                      'etc:%s' % (timer.str_estimated_complete()), flush=True)
+                validate(cls_model, val_data_loader)
+            else:
+                timer.reset_stage()
+        # Empty cache
+        torch.save(cls_model.module.state_dict(), laste_cam_weight_path)
+        torch.cuda.empty_cache()
 
 
 if __name__ == '__main__':
