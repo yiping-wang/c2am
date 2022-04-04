b393eac1dcd2abcd11b8a89082fdf49aea6d8bcf
Yiping Wang <yiping.wang@gmx.com>
2022-04-04 09:30:02 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -17,16 +17,20 @@
 
 
 echo "======= C2AM Reproduce ========="
-mkdir -p /data/home/yipingwang/data/C2AM/
-mkdir -p /data/home/yipingwang/data/IRLabelC2AM/
-mkdir -p /data/home/yipingwang/data/SemSegC2AM/
+mkdir -p /data/home/yipingwang/data/C2AMExp/
+mkdir -p /data/home/yipingwang/data/IRLabelC2AMExp/
+mkdir -p /data/home/yipingwang/data/SemSegC2AMExp/
 mkdir -p /data/home/yipingwang/data/GlobalCAM/
-rm /data/home/yipingwang/data/C2AM/*
-rm /data/home/yipingwang/data/IRLabelC2AM/*
-rm /data/home/yipingwang/data/SemSegC2AM/*
-python3 make_cam.py               --config    ./cfg/c2am_exp.yml
-python3 eval_cam.py               --config    ./cfg/c2am_exp.yml --cam_eval_thres 0.16
-python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp.yml
-python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp.yml
-python3 eval_sem_seg.py           --config    ./cfg/c2am_exp.yml
+rm /data/home/yipingwang/data/C2AMExp/*
+rm /data/home/yipingwang/data/IRLabelC2AMExp/*
+rm /data/home/yipingwang/data/SemSegC2AMExp/*
+python3 make_cam.py               --config    ./cfg/c2am_exp_00.yml
+python3 eval_cam.py               --config    ./cfg/c2am_exp_00.yml --cam_eval_thres 0.16
+python3 eval_cam.py               --config    ./cfg/c2am_exp_00.yml --cam_eval_thres 0.18
+python3 eval_cam.py               --config    ./cfg/c2am_exp_00.yml --cam_eval_thres 0.20
+python3 eval_cam.py               --config    ./cfg/c2am_exp_00.yml --cam_eval_thres 0.22
+python3 eval_cam.py               --config    ./cfg/c2am_exp_00.yml --cam_eval_thres 0.24
+# python3 cam_to_ir_label.py        --config    ./cfg/c2am_exp.yml
+# python3 make_sem_seg_labels.py    --config    ./cfg/c2am_exp.yml
+# python3 eval_sem_seg.py           --config    ./cfg/c2am_exp.yml
 echo "======================================="
