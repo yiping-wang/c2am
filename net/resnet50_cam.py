c9e9a1115b7c6916371d265a09df42427d141536
Yiping Wang <yiping.wang@gmx.com>
2022-04-07 03:16:59 -0400

    .

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -5,17 +5,6 @@ from misc import torchutils
 from net import resnet50
 
 
-class MLP(nn.Module):
-    def __init__(self, input_dim=2048, output_dim=128):
-        super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_dim, input_dim)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(input_dim, output_dim)
-
-    def forward(self, x):
-        return self.fc2(self.relu(self.fc1(x)))
-
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -55,6 +44,41 @@ class Net(nn.Module):
     def trainable_parameters(self):
         return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
 
+class PreCAM(nn.Module):
+    def __init__(self):
+        super(PreCAM, self).__init__()
+        self.resnet50 = resnet50.resnet50(
+            pretrained=True, strides=(2, 2, 2, 1))
+
+        self.stage1 = nn.Sequential(self.resnet50.conv1, self.resnet50.bn1, self.resnet50.relu, self.resnet50.maxpool,
+                                    self.resnet50.layer1)
+        self.stage2 = nn.Sequential(self.resnet50.layer2)
+        self.stage3 = nn.Sequential(self.resnet50.layer3)
+        self.stage4 = nn.Sequential(self.resnet50.layer4)
+
+        self.classifier = nn.Conv2d(2048, 20, 1, bias=False)
+
+        self.backbone = nn.ModuleList(
+            [self.stage1, self.stage2, self.stage3, self.stage4])
+        self.newly_added = nn.ModuleList([self.classifier])
+
+    def forward(self, x):
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+
+        x = F.conv2d(x, self.classifier.weight)
+        return x
+
+    def train(self, mode=True):
+        for p in self.resnet50.conv1.parameters():
+            p.requires_grad = False
+        for p in self.resnet50.bn1.parameters():
+            p.requires_grad = False
+
+    def trainable_parameters(self):
+        return (list(self.backbone.parameters()), list(self.newly_added.parameters()))
 
 class CAM(nn.Module):
     def __init__(self):
