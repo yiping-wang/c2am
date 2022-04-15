f3a47878a462fc8aa7b1f1cea631343431054d92
Yiping Wang <yiping.wang@gmx.com>
2022-04-15 01:56:57 -0400

    .

--- run_c2am.sh
+++ run_c2am.sh
@@ -3,11 +3,11 @@ mkdir -p /u8/y3967wang/output/CAM_C2AM/
 mkdir -p /u8/y3967wang/output/IRLabel_C2AM/
 mkdir -p /u8/y3967wang/output/SemSeg_C2AM/
 mkdir -p /u8/y3967wang/output/Global_CAM/
-rm /u8/y3967wang/output/CAM_C2AM/*
+# rm /u8/y3967wang/output/CAM_C2AM/*
 rm /u8/y3967wang/output/IRLabel_C2AM/*
 rm /u8/y3967wang/output/SemSeg_C2AM/*
 # python3 train_c2am.py             --config    ./cfg/c2am_reproduce.yml
-python3 make_cam.py               --config    ./cfg/c2am_reproduce.yml
+# python3 make_cam.py               --config    ./cfg/c2am_reproduce.yml
 python3 eval_cam.py               --config    ./cfg/c2am_reproduce.yml --cam_eval_thres 0.13
 python3 eval_cam.py               --config    ./cfg/c2am_reproduce.yml --cam_eval_thres 0.14
 python3 eval_cam.py               --config    ./cfg/c2am_reproduce.yml --cam_eval_thres 0.15
